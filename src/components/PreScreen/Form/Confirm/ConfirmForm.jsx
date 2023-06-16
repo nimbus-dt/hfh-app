0c64d9986d5a6db12dc1f6cf80605647c55cf084
Pedro Castro <pedroricardocastro2003@gmail.com>
2023-06-16 11:18:04 -0600

    Fix(Guille deleted)

---
+++ src/components/PreScreen/Form/Confirm/ConfirmForm.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Flex, Button, Heading, Card, Text } from '@aws-amplify/ui-react';
+import { Auth, DataStore } from 'aws-amplify';
+import { HouseholdData } from './HouseholdData';
+import {
+  HouseholdMember,
+  DebtRecord,
+  SavingRecord,
+  UserProps,
+} from '../../../../models';
+
+export function ConfirmForm() {
+  return (
+    <Card variation="elevated">
+      <Heading textAlign="center">Confirmation Form</Heading>
+
+      {/* Display user information */}
+      <Flex direction="column" gap="20px">
+        {/* Display user form data */}
+        {/* e.g., <UserForm userData={userData} /> */}
+      </Flex>
+
+      {/* Display household information */}
+      <Flex direction="column" gap="20px">
+        {/* Display household form data */}
+        {/* e.g., <HouseholdForm householdData={householdData} /> */}
+      </Flex>
+
+      {/* Display savings information */}
+      <Flex direction="column" gap="20px">
+        {/* Display savings form data */}
+        {/* e.g., <SavingsForm savingsData={savingsData} /> */}
+      </Flex>
+
+      {/* Display debt information */}
+      <Flex direction="column" gap="20px">
+        {/* Display debt form data */}
+        {/* e.g., <DebtForm debtData={debtData} /> */}
+      </Flex>
+
+      {/* Add a button to submit the form */}
+      <Button type="submit" variation="primary">
+        Confirm
+      </Button>
+    </Card>
+  );
+}
+
+export default ConfirmForm;
