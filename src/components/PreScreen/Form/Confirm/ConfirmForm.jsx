9accf4bd0b70c04d2194f6379062edabb0dd82bf
Pedro Castro <pedroricardocastro2003@gmail.com>
2023-06-16 11:12:37 -0600

    confirm layout

--- src/components/PreScreen/Form/Confirm/ConfirmForm.jsx
+++ src/components/PreScreen/Form/Confirm/ConfirmForm.jsx
@@ -10,81 +10,32 @@ import {
 } from '../../../../models';
 
 export function ConfirmForm() {
-  const [user, setUser] = useState(null);
-  const [householdMembers, setHouseholdMembers] = useState([]);
-  const [debts, setDebts] = useState([]);
-  const [savings, setSavings] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const currentUser = await Auth.currentAuthenticatedUser();
-        const userData = await DataStore.query(
-          UserProps,
-          currentUser.attributes.sub
-        );
-        const householdMembersData = await DataStore.query(HouseholdMember);
-        const debtsData = await DataStore.query(DebtRecord);
-        const savingsData = await DataStore.query(SavingRecord);
-
-        setUser(userData);
-        setHouseholdMembers(householdMembersData);
-        setDebts(debtsData);
-        setSavings(savingsData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (!user || !householdMembers.length || !debts.length || !savings.length) {
-    return <text> Loading...</text>; // Return a loading state or placeholder while data is being fetched
-  }
-
   return (
     <Card variation="elevated">
       <Heading textAlign="center">Confirmation Form</Heading>
 
       {/* Display user information */}
-      <Flex direction="column" gap="10px">
-        <Text>{`Name: ${user.name}`}</Text>
-        <Text>{`Last Name: ${user.lastName}`}</Text>
-        <Text>{`Sex: ${user.sex}`}</Text>
-        <Text>{`Phone: ${user.phone}`}</Text>
-        <Text>{`Address: ${user.address}`}</Text>
-        <Text>{`Zip: ${user.zip}`}</Text>
+      <Flex direction="column" gap="20px">
+        {/* Display user form data */}
+        {/* e.g., <UserForm userData={userData} /> */}
       </Flex>
 
       {/* Display household information */}
       <Flex direction="column" gap="20px">
-        <Heading level="3">Household Information</Heading>
-        <HouseholdData householdMembers={householdMembers} />
+        {/* Display household form data */}
+        {/* e.g., <HouseholdForm householdData={householdData} /> */}
       </Flex>
 
       {/* Display savings information */}
       <Flex direction="column" gap="20px">
-        <Heading level="3">Savings Information</Heading>
-        {savings.map((savingsRecord) => (
-          <Card key={savingsRecord.id} variation="outlined">
-            <Text>{`Owner: ${savingsRecord.ownerID}`}</Text>
-            <Text>{`Institution: ${savingsRecord.institution}`}</Text>
-            <Text>{`Estimated Amount: ${savingsRecord.estimatedAmount}`}</Text>
-          </Card>
-        ))}
+        {/* Display savings form data */}
+        {/* e.g., <SavingsForm savingsData={savingsData} /> */}
       </Flex>
 
       {/* Display debt information */}
       <Flex direction="column" gap="20px">
-        <Heading level="3">Debt Information</Heading>
-        {debts.map((debtRecord) => (
-          <Card key={debtRecord.id} variation="outlined">
-            <Text>{`Monthly Recurrence: ${debtRecord.monthlyRecurrence}`}</Text>
-            <Text>{`Type of Debt: ${debtRecord.typeOfDebt}`}</Text>
-            <Text>{`Estimated Amount: ${debtRecord.estimatedAmount}`}</Text>
-          </Card>
-        ))}
+        {/* Display debt form data */}
+        {/* e.g., <DebtForm debtData={debtData} /> */}
       </Flex>
 
       {/* Add a button to submit the form */}
