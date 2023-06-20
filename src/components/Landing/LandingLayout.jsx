fe27640816ab1d8dea26885504b95ae4ffc61855
Guille <joseavelarcaballero@gmail.com>
2023-06-20 17:18:07 -0600

    Small fix

--- src/components/Landing/LandingLayout.jsx
+++ src/components/Landing/LandingLayout.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unresolved */
-/* eslint-disable import/no-extraneous-dependencies */import { DataStore } from 'aws-amplify';
+/* eslint-disable import/no-extraneous-dependencies */
+import { DataStore } from 'aws-amplify';
 import {
   Flex,
   Button,
