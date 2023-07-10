cfd06e8d5bc4849f242a34d6f0d40c2b58c016b0
Guille <joseavelarcaballero@gmail.com>
2023-07-10 10:43:30 -0600

    Lambda changes

--- amplify/backend/function/prescreennotify/src/index.js
+++ amplify/backend/function/prescreennotify/src/index.js
@@ -1,12 +1,48 @@
+/* eslint-disable import/no-relative-packages */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { DataStore } from 'aws-amplify';
+import { UserProps } from '../../../../../src/models';
+
+// eslint-disable-next-line import/no-unresolved
+const aws = require('aws-sdk');
+
+const ses = new aws.SES({ region: 'us-east-1' });
+
+let userProps;
+
 exports.handler = async (event) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  for (const record of event.Records) {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+  for (const streamedItem of event.Records) {
+    if (streamedItem.eventName === 'INSERT') {
+      const ownerID = streamedItem.dynamodb.NewImage.ownerID.S;
+
+      try {
+        userProps = await DataStore.query(UserProps, (c) =>
+          c.ownerID.eq(ownerID)
+        );
+      } catch (error) {
+        console.log(`Error sending email: ${error}`);
+      }
+
+      await ses
+        .sendEmail({
+          Destination: {
+            ToAddresses: ['jgavelarc@gmail.com'],
+          },
+          Source: process.env.SENDER_EMAIL,
+          Message: {
+            Subject: { Data: 'Prescreen Notification' },
+            Body: {
+              Text: {
+                Data: `Hi ${ownerID},\n\nYour prescreen has been scheduled. Please check your calendar for details.\n\nThanks,\nYour Recruiting Team`,
+              },
+            },
+          },
+        })
+        .promise();
+    }
   }
-  return Promise.resolve('Successfully processed DynamoDB record');
+  return { status: 'done' };
 };
