0c14e0e3a6b5f43c44fe573e3f4a92a470bb4edb
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-01 10:43:52 -0600

    ✨ show multiple files in income detail

--- src/components/PreScreen/Form/Income/IncomeDetail.jsx
+++ src/components/PreScreen/Form/Income/IncomeDetail.jsx
@@ -1,34 +1,13 @@
-/* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/prop-types */
-import { Card, Flex, Text, Heading, Link } from '@aws-amplify/ui-react';
+import { Card, Flex, Text, Heading, Button } from '@aws-amplify/ui-react';
+import { HiXMark } from 'react-icons/hi2';
 import { useEffect, useState } from 'react';
 import { DataStore, Storage } from 'aws-amplify';
 import { HouseholdMember, IncomeRecord, UserProps } from '../../../../models';
+import { downloadWithUrl } from '../../../../utils/files';
 
 export function IncomeDetail({ item, sizeRenderer, application }) {
   const [names, setNames] = useState({});
-  const [file, setFile] = useState(null);
-
-  useEffect(() => {
-    async function getFile() {
-      const owner = application?.ownerID;
-
-      // get user props
-      const userProps = await DataStore.query(UserProps, (c) =>
-        c.ownerID.eq(owner)
-      );
-
-      const identity = userProps[0]?.identityID;
-
-      const result = await Storage.get(item.proofOfIncome[0], {
-        level: 'protected',
-        identityId: identity,
-      });
-
-      setFile(result);
-    }
-    getFile();
-  }, [application?.ownerID, item.proofOfIncome]);
 
   useEffect(() => {
     const fetchOwner = async () => {
@@ -63,7 +42,7 @@ export function IncomeDetail({ item, sizeRenderer, application }) {
     fetchOwner();
   }, [item.ownerID]);
 
-  async function deleteObject() {
+  const deleteObject = async () => {
     try {
       await DataStore.delete(IncomeRecord, item.id);
       window.location.reload();
@@ -73,30 +52,87 @@ export function IncomeDetail({ item, sizeRenderer, application }) {
         error
       );
     }
-  }
+  };
+
+  const downloadFile = async (fileKey) => {
+    const owner = application?.ownerID;
+
+    // get user props
+    const userProps = await DataStore.query(UserProps, (c) =>
+      c.ownerID.eq(owner)
+    );
+
+    const identity = userProps[0]?.identityID;
+
+    const signedURL = await Storage.get(fileKey, {
+      level: 'protected',
+      identityId: identity,
+    });
+
+    downloadWithUrl(signedURL);
+  };
 
   return (
-    <Card variation="elevated" width={sizeRenderer ? '80%' : '300px'}>
+    <Card variation="elevated" width={sizeRenderer ? '100%' : '300px'}>
       <Flex direction="column" gap="1px">
-        <Heading level="4">Owner: {names.name}</Heading>
+        <Flex justifyContent="space-between" gap="0.5rem">
+          <Heading level="4">Owner: {names.name}</Heading>
+
+          <Button
+            title="Remove income record"
+            shrink="0rem"
+            type="button"
+            variation="warning"
+            height="2rem"
+            width="2rem"
+            padding="0rem"
+            onClick={deleteObject}
+          >
+            <HiXMark size={20} />
+          </Button>
+        </Flex>
+
         <Flex gap="5px">
           <Text fontWeight="bold">Employer:</Text>
           <Text>{item.employer}</Text>
         </Flex>
+
         <Flex gap="5px">
           <Text fontWeight="bold">Monthly income:</Text>
           <Text>${parseInt(item.estimatedMonthlyIncome)}</Text>
         </Flex>
+
         <Flex gap="5px">
           <Text fontWeight="bold">Employment Time:</Text>
           <Text>{parseInt(item.employmentTime)} months</Text>
         </Flex>
-        <Flex gap="5px">
-          <Link fontWeight="bold" href={file}>
-            Proof of Income
-          </Link>
+
+        <Flex direction="column" gap="5px">
+          <Text fontWeight="bold">Proof of Income:</Text>
+
+          <Flex
+            direction="column"
+            gap="0.25rem"
+            maxHeight="7rem"
+            overflow="auto"
+          >
+            <ul style={{ margin: 0 }}>
+              {item.proofOfIncome.map((fileKey, index) => (
+                <li key={fileKey}>
+                  <Button
+                    type="button"
+                    title={`Download file #${index + 1}`}
+                    variation="link"
+                    padding="0rem 0.55rem 0rem 0.55rem"
+                    onClick={() => downloadFile(fileKey)}
+                  >
+                    File #{index + 1}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </Flex>
         </Flex>
-        <Link onClick={deleteObject}>Delete</Link>
       </Flex>
     </Card>
   );
