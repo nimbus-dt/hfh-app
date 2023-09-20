1233eaabb757fbeaa4a6466970bd08aa2885f5cd
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-20 16:14:24 -0600

    ✨ add formatSexValue utility

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,3 +1,5 @@
+import { SEX_VALUES_MAP } from './constants';
+
 /**
  * Returns a string with all the clases joined by empty space
  * @param {...string} classes
@@ -5,3 +7,10 @@
  */
 export const concatClassNames = (...classes) =>
   classes.filter(Boolean).join(' ');
+
+/**
+ * Returns a string with the sex value as a human readable string
+ * @param { SexTypes } sexType
+ * @returns {string}
+ */
+export const formatSexValue = (sexType) => SEX_VALUES_MAP[sexType] ?? '';
