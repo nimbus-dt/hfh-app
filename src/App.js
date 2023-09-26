868f63ede606956a71e48d381611289cccbac4b1
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-26 09:46:09 -0600

    🚚 move PreLimHome to pages directory

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './assets/styles/App.css';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import { Authenticator, ScrollView } from '@aws-amplify/ui-react';
 import FormApplicationsPage from 'pages/applicant/prescreen/form/apps';
+import PreLimHomePage from 'pages/applicant/prescreen/prelim/home';
 import { ApplicantPrescreenLayout } from './components/PreScreen/ApplicantPrescreenLayout';
 import { PreLimLayout } from './components/PreScreen/PreLim/PreLimLayout';
 
 // eslint-disable-next-line import/no-unresolved
 import '@aws-amplify/ui-react/styles.css';
-import { PreLimHome } from './components/PreScreen/PreLim/PreLimHome';
 import { PreLimTerms } from './components/PreScreen/PreLim/PreLimTerms';
 import { PreLimQuestions } from './components/PreScreen/PreLim/PreLimQuestions';
 import { PreLimResults } from './components/PreScreen/PreLim/PreLimResults';
@@ -97,7 +97,7 @@ function App() {
         <Route path=":habitat">
           <Route path="prescreen" element={<ApplicantPrescreenLayout />}>
             <Route path="prelim" element={<PreLimLayout />}>
-              <Route path="home" element={<PreLimHome />} />
+              <Route path="home" element={<PreLimHomePage />} />
               <Route path="terms" element={<PreLimTerms />} />
               <Route path="questions" element={<PreLimQuestions />} />
               <Route path="results" element={<PreLimResults />} />
