1ca6c7327fae76bc123017a09b8c2f9ffaa0a752
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-27 09:46:20 -0600

    ✨ add useHabitatByUrlName hook

---
+++ src/hooks/services/useHabitatByUrlName/useHabitatByUrlName.js
@@ -0,0 +1,26 @@
+import { Habitat } from 'models';
+import useDataStoreQuery from 'hooks/services/useDataStoreQuery';
+import { SortDirection } from 'aws-amplify';
+
+/**
+ * Wrapper to fetch Habitat by urlName
+ * @param {Object} params
+ * @param {string} params.habitatUrlName
+ */
+export function useHabitatByUrlName({ habitatUrlName }) {
+  const { data, loading, error } = useDataStoreQuery({
+    model: Habitat,
+    criteria: (c) => c.urlName.eq(habitatUrlName),
+    paginationProducer: {
+      sort: (s) => s.urlName(SortDirection.ASCENDING),
+      page: 0,
+      limit: 10,
+    },
+  });
+
+  return {
+    habitat: data?.[0] ?? {},
+    loading,
+    error,
+  };
+}
