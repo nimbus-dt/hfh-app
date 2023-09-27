3ea779cc6f075461b24fdc9ee8823c27c00cdbf2
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-27 09:46:52 -0600

    ♻️ refactor PreLimLayout

--- src/layouts/PreLimLayout/PreLimLayout.jsx
+++ src/layouts/PreLimLayout/PreLimLayout.jsx
@@ -1,40 +1,17 @@
 import { useParams, Outlet } from 'react-router-dom';
-import { Flex, Heading, Card, Divider } from '@aws-amplify/ui-react';
-import { useState, useEffect } from 'react';
-import { DataStore } from '@aws-amplify/datastore';
-import { Habitat } from 'models';
+import { Flex, Card, Divider } from '@aws-amplify/ui-react';
+import useHabitatByUrlName from 'hooks/services/useHabitatByUrlName';
+import { LayoutHeader } from './LayoutHeader';
 
 export function PreLimLayout() {
-  const [habitat, setHabitat] = useState({});
-  const urlName = useParams().habitat;
+  const { habitat: habitatUrlName } = useParams();
+  const { habitat, error } = useHabitatByUrlName({
+    habitatUrlName,
+  });
 
-  useEffect(() => {
-    async function fetchHabitat() {
-      try {
-        const habitatObject = await DataStore.query(Habitat, (c) =>
-          c.urlName.eq(urlName)
-        );
-        setHabitat(habitatObject[0]);
-      } catch (error) {
-        console.log('Error retrieving Habitat', error);
-      }
-    }
-    fetchHabitat();
-  }, [urlName]);
-
-  const title = (
-    <Flex direction="column">
-      <Heading level={3} fontWeight="bold">
-        {habitat?.name}
-      </Heading>
-      <Heading level={3} marginTop="-10px" fontWeight="bold">
-        Habitat for Humanity
-      </Heading>
-      <Heading level={3} marginTop="-10px" fontWeight="bold">
-        PreScreen Form
-      </Heading>
-    </Flex>
-  );
+  if (error) {
+    console.log('Error retrieving Habitat:', error.message);
+  }
 
   return (
     <Card
@@ -44,10 +21,13 @@ export function PreLimLayout() {
       marginBottom="30px"
     >
       <Divider marginBottom="10px" />
+
       <Flex direction="column" gap="xl">
-        {title}
-        <Outlet context={[habitat, setHabitat]} />
+        <LayoutHeader habitatName={habitat?.name} />
+
+        <Outlet context={[habitat]} />
       </Flex>
+
       <Divider marginTop="20px" />
     </Card>
   );
