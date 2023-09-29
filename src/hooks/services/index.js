225788db87c19f450c6103dabea1abf1337ee370
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-09-29 08:47:54 -0600

    ✨ add hook services

--- src/hooks/services/index.js
+++ src/hooks/services/index.js
@@ -1,9 +1,44 @@
 import {
   Application,
+  DebtRecord,
+  HouseholdMember,
+  IncomeRecord,
+  SavingRecord,
 } from 'models';
 import { dataStoreQueryHookBuilder } from './useDataStoreQuery/useDataStoreQuery';
+import { dataStoreQueryByIdHookBuilder } from './useDataStoreQueryById/useDataStoreQueryById';
+
 /* APPLICATION MODEL SERVICES */
 export const useApplicationsQuery = dataStoreQueryHookBuilder({
   model: Application,
+  defaultDataValue: [],
+});
+
+export const useApplicationById = dataStoreQueryByIdHookBuilder({
+  model: Application,
+  defaultDataValue: null,
+});
+
+/* SAVING RECORD MODEL SERVICES */
+export const useSavingRecordsQuery = dataStoreQueryHookBuilder({
+  model: SavingRecord,
+  defaultDataValue: [],
+});
+
+/* INCOME RECORD MODEL SERVICES */
+export const useIncomeRecordsQuery = dataStoreQueryHookBuilder({
+  model: IncomeRecord,
+  defaultDataValue: [],
+});
+
+/* DEBT RECORD MODEL SERVICES */
+export const useDebtRecordsQuery = dataStoreQueryHookBuilder({
+  model: DebtRecord,
+  defaultDataValue: [],
+});
+
+/* HOUSEHOLD MEMBER MODEL SERVICES */
+export const useHouseholdMembersQuery = dataStoreQueryHookBuilder({
+  model: HouseholdMember,
   defaultDataValue: [],
 });
