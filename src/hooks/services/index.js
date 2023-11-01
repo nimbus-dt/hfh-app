41cfcc2435301c877b83140d47a984d1769eccfb
Pablo Avelar <jp.avelar.m96@gmail.com>
2023-10-31 23:03:40 -0600

    ✨ add observeQuery hooks services

--- src/hooks/services/index.js
+++ src/hooks/services/index.js
@@ -7,6 +7,7 @@ import {
 } from 'models';
 import { dataStoreQueryHookBuilder } from './useDataStoreQuery/useDataStoreQuery';
 import { dataStoreQueryByIdHookBuilder } from './useDataStoreQueryById/useDataStoreQueryById';
+import { dataStoreObserveQueryHookBuilder } from './useDataStoreObserveQuery/useDataStoreObserveQuery';
 
 /* APPLICATION MODEL SERVICES */
 export const useApplicationsQuery = dataStoreQueryHookBuilder({
@@ -25,8 +26,18 @@ export const useSavingRecordsQuery = dataStoreQueryHookBuilder({
   defaultDataValue: [],
 });
 
+export const useSavingRecordsObserveQuery = dataStoreObserveQueryHookBuilder({
+  model: SavingRecord,
+  defaultDataValue: [],
+});
+
 /* INCOME RECORD MODEL SERVICES */
 export const useIncomeRecordsQuery = dataStoreQueryHookBuilder({
+  model: IncomeRecord,
+  defaultDataValue: [],
+});
+
+export const useIncomeRecordsObserveQuery = dataStoreObserveQueryHookBuilder({
   model: IncomeRecord,
   defaultDataValue: [],
 });
@@ -37,8 +48,20 @@ export const useDebtRecordsQuery = dataStoreQueryHookBuilder({
   defaultDataValue: [],
 });
 
+export const useDebtRecordsObserveQuery = dataStoreObserveQueryHookBuilder({
+  model: DebtRecord,
+  defaultDataValue: [],
+});
+
 /* HOUSEHOLD MEMBER MODEL SERVICES */
 export const useHouseholdMembersQuery = dataStoreQueryHookBuilder({
   model: HouseholdMember,
   defaultDataValue: [],
 });
+
+export const useHouseholdMembersObserveQuery = dataStoreObserveQueryHookBuilder(
+  {
+    model: HouseholdMember,
+    defaultDataValue: [],
+  }
+);
