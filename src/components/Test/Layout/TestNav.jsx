332622740c5a8fa23cf971afdf0892c39ca2c462
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-04 15:33:57 -0600

    💄 ✨ Update TestNav to add sign out button

--- src/components/Test/Layout/TestNav.jsx
+++ src/components/Test/Layout/TestNav.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
-import { Image, Card, Flex } from '@aws-amplify/ui-react';
+import { Image, Card, Flex, Button } from '@aws-amplify/ui-react';
+import PropTypes from 'prop-types';
+import { Auth } from 'aws-amplify';
 import logoHabitat from '../../../assets/images/logoHabitat.svg';
 
-export function TestNav() {
+export function TestNav({ isAuthenticated }) {
+  const handleSignOut = async () => {
+    await Auth.signOut();
+  };
+
   return (
     <Card wrap width="100%" backgroundColor="#55B949" padding="0">
       <Flex direction="row" justifyContent="space-between" alignItems="center">
@@ -12,7 +18,21 @@ export function TestNav() {
           height="100%"
           marginLeft="-10px"
         />
+        {isAuthenticated && (
+          <Button
+            variation="link"
+            color="black"
+            marginRight="1rem"
+            onClick={handleSignOut}
+          >
+            Sign out
+          </Button>
+        )}
       </Flex>
     </Card>
   );
 }
+
+TestNav.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
