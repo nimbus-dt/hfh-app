8bcb21e4707850bd6ea7d40c8dcb00919ce4b41a
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-12 14:29:44 -0600

    ✨ Add FinancialSection TestReview Component

---
+++ src/components/Test/Parts/TestReview/components/FinancialSection/FinancialSection.jsx
@@ -0,0 +1,179 @@
+import {
+  Flex,
+  Tabs,
+  TabItem,
+  ScrollView,
+  View,
+  Text,
+  Loader,
+} from '@aws-amplify/ui-react';
+import { useOutletContext } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
+import { DataStore } from 'aws-amplify';
+import { ApplicantInfo, Member, Income, Debt, Asset } from 'models';
+import { CustomCard } from 'components/Test/Reusable/CustomCard';
+import IncomeSection from './components/IncomeSection';
+import DebtSection from './components/DebtSection';
+import AssetsSection from './components/AssetsSection';
+
+export default function FinancialSection() {
+  const [applicantInfo, setApplicantInfo] = useState();
+  const [members, setMembers] = useState([]);
+  const [incomes, setIncomes] = useState([]);
+  const [debts, setDebts] = useState([]);
+  const [assets, setAssets] = useState([]);
+  const [selectedTab, setSelectedTab] = useState(0);
+  const { application } = useOutletContext();
+
+  const handleSelectedTabOnChange = (newTab) => setSelectedTab(Number(newTab));
+
+  const ownerBySelectedTab = useMemo(() => {
+    if (applicantInfo !== undefined) {
+      return selectedTab === 0
+        ? {
+            id: applicantInfo.id,
+            fullName: applicantInfo.props.basicInfo.fullName,
+          }
+        : {
+            id: members[selectedTab - 1].id,
+            fullName: members[selectedTab - 1].props.fullName,
+          };
+    }
+    return undefined;
+  }, [applicantInfo, members, selectedTab]);
+
+  const filterIncomesBySelectedTab = useMemo(
+    () => incomes.filter((income) => income.ownerId === ownerBySelectedTab.id),
+    [incomes, ownerBySelectedTab]
+  );
+
+  const filterDebtsBySelectedTab = useMemo(
+    () => debts.filter((debt) => debt.ownerId === ownerBySelectedTab.id),
+    [debts, ownerBySelectedTab]
+  );
+
+  const filterAssetsBySelectedTab = useMemo(
+    () => assets.filter((asset) => asset.ownerId === ownerBySelectedTab.id),
+    [assets, ownerBySelectedTab]
+  );
+
+  useEffect(() => {
+    const getFinancial = async (applicationID) => {
+      try {
+        const existingMembers = await DataStore.query(Member, (c) =>
+          c.testapplicationID.eq(applicationID)
+        );
+        setMembers(existingMembers);
+        const existingApplicantInfo = await DataStore.query(
+          ApplicantInfo,
+          (c) => c.ownerID.eq(applicationID)
+        );
+        setApplicantInfo(existingApplicantInfo[0]);
+        const membersIdArray = existingMembers.map(
+          (existingMember) => existingMember.id
+        );
+
+        const applicantAndMembersIdArray = [
+          existingApplicantInfo[0].id,
+          ...membersIdArray,
+        ];
+
+        const existingIncomes = await DataStore.query(Income, (c) =>
+          c.or((c2) => {
+            const arrayOfFilters = applicantAndMembersIdArray.map((id) =>
+              c2.ownerId.eq(id)
+            );
+
+            return arrayOfFilters;
+          })
+        );
+
+        setIncomes(existingIncomes);
+
+        const existingDebts = await DataStore.query(Debt, (c) =>
+          c.or((c2) => {
+            const arrayOfFilters = applicantAndMembersIdArray.map((id) =>
+              c2.ownerId.eq(id)
+            );
+
+            return arrayOfFilters;
+          })
+        );
+
+        setDebts(existingDebts);
+
+        const existingAssets = await DataStore.query(Asset, (c) =>
+          c.or((c2) => {
+            const arrayOfFilters = applicantAndMembersIdArray.map((id) =>
+              c2.ownerId.eq(id)
+            );
+
+            return arrayOfFilters;
+          })
+        );
+
+        setAssets(existingAssets);
+      } catch (error) {
+        console.log('Error fetching the financial data.', error);
+      }
+    };
+
+    if (application) {
+      getFinancial(application.id);
+    }
+  }, [application]);
+
+  return applicantInfo === undefined ? (
+    <CustomCard>
+      <Flex>
+        <Loader size="large" />
+        <Text>Loading financial data</Text>
+      </Flex>
+    </CustomCard>
+  ) : (
+    <>
+      <View
+        width={{ base: '80%', medium: '500px' }}
+        borderRadius="medium"
+        overflow="hidden"
+      >
+        <ScrollView backgroundColor="white">
+          <Tabs
+            spacing="equal"
+            whiteSpace="nowrap"
+            currentIndex={selectedTab}
+            onChange={handleSelectedTabOnChange}
+          >
+            <TabItem title={applicantInfo?.props.basicInfo.fullName} />
+            {members.map((member) => (
+              <TabItem key={member.id} title={member?.props.fullName} />
+            ))}
+          </Tabs>
+        </ScrollView>
+      </View>
+      <br />
+      <CustomCard>
+        <Text fontWeight="bold">
+          {ownerBySelectedTab !== undefined &&
+            `${ownerBySelectedTab.fullName} Financial Information`}
+        </Text>
+      </CustomCard>
+      <br />
+      <IncomeSection
+        ownerId={ownerBySelectedTab?.id}
+        incomes={filterIncomesBySelectedTab}
+      />
+      <br />
+      <DebtSection
+        ownerId={ownerBySelectedTab?.id}
+        debts={filterDebtsBySelectedTab}
+      />
+      <br />
+      <AssetsSection
+        ownerId={ownerBySelectedTab?.id}
+        assets={filterAssetsBySelectedTab}
+      />
+      <br />
+    </>
+  );
+}
