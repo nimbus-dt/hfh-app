a4fede3a1f014a04dab0be54bf0dae1a74f2bd68
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-19 19:23:24 -0600

    ✨ Add ApplicationMetricsTable Component

---
+++ src/pages/affiliate-portal/applications/:applicationId/components/ApplicationMetricsTable/ApplicationMetricsTable.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import DataTable from 'components/DataTable';
+import {
+  formatNumberAsCurrency,
+  formatNumberAsPercentage,
+} from 'utils/formatters';
+
+export function ApplicationMetricsTable({
+  totalMonthlyIncomes,
+  totalAssets,
+  totalMonthlyDebts,
+  totalDebts,
+  debtToIncomeRatio,
+}) {
+  const applicantData = [
+    {
+      header: 'Total Monthly Household Income',
+      value: formatNumberAsCurrency(totalMonthlyIncomes ?? 0),
+    },
+    {
+      header: 'Total Monthly Household Debt',
+      value: formatNumberAsCurrency(totalMonthlyDebts ?? 0),
+    },
+    {
+      header: 'Total hoysehold Debt',
+      value: formatNumberAsCurrency(totalDebts ?? 0),
+    },
+    {
+      header: 'Total Household Asset Value',
+      value: formatNumberAsCurrency(totalAssets ?? 0),
+    },
+    {
+      header: 'Debt to Income Ratio',
+      value: formatNumberAsPercentage(debtToIncomeRatio ?? 0),
+    },
+  ];
+
+  return <DataTable heading="Applicant Information" data={applicantData} />;
+}
+
+ApplicationMetricsTable.propTypes = {
+  totalMonthlyIncomes: PropTypes.number,
+  totalAssets: PropTypes.number,
+  totalMonthlyDebts: PropTypes.number,
+  totalDebts: PropTypes.number,
+  debtToIncomeRatio: PropTypes.number,
+};
