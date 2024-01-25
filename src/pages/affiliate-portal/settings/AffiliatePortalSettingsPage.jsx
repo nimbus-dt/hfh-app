c7eadf3c33076174f8385d823e968e600312ac8b
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-25 10:41:01 -0600

    ✨ Add AffiliatePortalSettingsPage Component

---
+++ src/pages/affiliate-portal/settings/AffiliatePortalSettingsPage.jsx
@@ -0,0 +1,20 @@
+import { Flex, Heading, Divider, Badge } from '@aws-amplify/ui-react';
+
+export function AffiliatePortalSettingsPage() {
+  return (
+    <Flex
+      direction="column"
+      width="100%"
+      alignContent="center"
+      justifyContent="center"
+    >
+      <Heading level={3} fontWeight="bold" textAlign="center">
+        Settings
+      </Heading>
+      <Divider />
+      <Badge variation="warning" width="100%">
+        This page is currently under construction
+      </Badge>
+    </Flex>
+  );
+}
