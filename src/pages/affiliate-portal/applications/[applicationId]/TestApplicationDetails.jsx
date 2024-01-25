26ab7e076ed365dc10661a1788443b2ddcfee85f
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-24 20:30:03 -0600

    ✨ Update TestApplicationDetails to have return and decide buttons

--- src/pages/affiliate-portal/applications/[applicationId]/TestApplicationDetails.jsx
+++ src/pages/affiliate-portal/applications/[applicationId]/TestApplicationDetails.jsx
@@ -6,6 +6,9 @@ import {
   ScrollView,
   Tabs,
   TabItem,
+  Text,
+  TextAreaField,
+  SelectField,
 } from '@aws-amplify/ui-react';
 import {
   useApplicantInfosQuery,
@@ -27,6 +30,11 @@ import {
   getTotalAssetsValue,
 } from 'utils/applicationMetrics';
 import { useState } from 'react';
+import Modal from 'components/Modal';
+import { API, DataStore } from 'aws-amplify';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TestApplication } from 'models';
 import ApplicantInfoTable from './components/ApplicantInfoTable';
 import GeneralInfoTable from './components/GeneralInfoTable';
 import ChecklistTable from './components/ChecklistTable';
@@ -36,15 +44,18 @@ import EmploymentTable from './components/EmploymentTable';
 import ApplicationMetricsTable from './components/ApplicationMetricsTable';
 import HouseholdTable from './components/HouseholdTable';
 import FinancialSection from './components/FinancialSection';
+import { decideSchema, returnSchema } from '../TestApplicationDetails.schema';
 
 const TestApplicationDetails = () => {
   const [selectedTab, setSelectedTab] = useState(0);
-
-  const handleSelectedTabOnChange = (newTab) => setSelectedTab(Number(newTab));
+  const [trigger, setTrigger] = useState(true);
+  const [returnModalOpen, setReturnModalOpen] = useState(false);
+  const [decideModalOpen, setDecideModalOpen] = useState(false);
   const { habitat } = useOutletContext();
   const { applicationId } = useParams();
   const { data: application } = useTestApplicationById({
     id: applicationId,
+    dependencyArray: [trigger],
   });
   const queriesProps2 = {
     criteria: (c1) => c1.ownerID.eq(application?.id),
@@ -92,10 +103,88 @@ const TestApplicationDetails = () => {
   );
   const navigate = useNavigate();
 
+  const {
+    register: registerReturn,
+    handleSubmit: handleSubmitReturn,
+    reset: resetReturn,
+    formState: { errors: errorsReturn },
+  } = useForm({
+    resolver: zodResolver(returnSchema),
+  });
+
+  const {
+    register: registerDecide,
+    handleSubmit: handleSubmitDecide,
+    reset: resetDecide,
+    formState: { errors: errorsDecide },
+  } = useForm({
+    resolver: zodResolver(decideSchema),
+  });
+
   const sizeRenderer = useBreakpointValue({
     base: true,
     large: false,
   });
+
+  const handleSelectedTabOnChange = (newTab) => setSelectedTab(Number(newTab));
+
+  const handleReturnOnClick = () => setReturnModalOpen(true);
+
+  const handleReturnModalOnClose = () => setReturnModalOpen(false);
+
+  const handleOnValidReturn = async (data) => {
+    try {
+      const original = await DataStore.query(TestApplication, application.id);
+      const persistedApplication = await DataStore.save(
+        TestApplication.copyOf(original, (originalApplication) => {
+          originalApplication.submitted = false;
+        })
+      );
+
+      await API.post('sendEmailToApplicantAPI', '/notify', {
+        body: {
+          subject: `Status update on your ${habitat?.name} application`,
+          body: data.message,
+          sub: persistedApplication.ownerID,
+        },
+      });
+
+      setReturnModalOpen(false);
+      setTrigger((previousTrigger) => !previousTrigger);
+      resetReturn();
+    } catch (error) {
+      console.log('An error ocurred while returning the application');
+    }
+  };
+
+  const handleDecideOnClick = () => setDecideModalOpen(true);
+
+  const handleDecideModalOnClose = () => setDecideModalOpen(false);
+
+  const handleOnValidDecide = async (data) => {
+    try {
+      const original = await DataStore.query(TestApplication, application.id);
+      const persistedApplication = await DataStore.save(
+        TestApplication.copyOf(original, (originalApplication) => {
+          originalApplication.status = data.status;
+        })
+      );
+
+      await API.post('sendEmailToApplicantAPI', '/notify', {
+        body: {
+          subject: `Status update on your ${habitat?.name} application`,
+          body: data.message,
+          sub: persistedApplication.ownerID,
+        },
+      });
+
+      setDecideModalOpen(false);
+      setTrigger((previousTrigger) => !previousTrigger);
+      resetDecide();
+    } catch (error) {
+      console.log('An error ocurred while returning the application');
+    }
+  };
 
   return (
     <Flex width="auto" direction="column">
@@ -175,9 +264,84 @@ const TestApplicationDetails = () => {
           debtToIncomeRatio={debtToIncomeRatio}
         />
       )}
+      <Modal
+        title="Return"
+        open={returnModalOpen}
+        onClickClose={handleReturnModalOnClose}
+        width="30rem"
+      >
+        <form onSubmit={handleSubmitReturn(handleOnValidReturn)}>
+          <Text>
+            By returning an application you are giving an applicant the chance
+            to edit their info.
+          </Text>
 
-      <Button onClick={() => {}}>Accept</Button>
-      <Button onClick={() => {}}>Reject</Button>
+          <TextAreaField
+            {...registerReturn('message')}
+            label="Return message"
+            descriptiveText="We will email this to the user"
+            placeholder="We are returning your application because you lack correct financial records. For inquiries, email support@test-habitat.com"
+            rows={3}
+            hasError={errorsReturn?.message}
+            errorMessage="Invalid message"
+          />
+          <Flex justifyContent="end" marginTop="1rem">
+            <Button variation="destructive" onClick={handleReturnModalOnClose}>
+              Cancel
+            </Button>
+            <Button type="submit">Return</Button>
+          </Flex>
+        </form>
+      </Modal>
+      <Modal
+        title="Decide"
+        open={decideModalOpen}
+        onClickClose={handleDecideModalOnClose}
+        width="30rem"
+      >
+        <form onSubmit={handleSubmitDecide(handleOnValidDecide)}>
+          <Text>
+            Here is where you can render a decision for an application.
+          </Text>
+          <SelectField
+            {...registerDecide('status')}
+            label="Status"
+            hasError={errorsDecide?.status}
+            errorMessage="Invalid status"
+          >
+            <option value="Unset">Unset</option>
+            {(habitat?.props.data.customStatus
+              ? habitat.props.data.customStatus
+              : []
+            ).map((customStatusItem) => (
+              <option key={customStatusItem} value={customStatusItem}>
+                {customStatusItem}
+              </option>
+            ))}
+          </SelectField>
+          <TextAreaField
+            {...registerDecide('message')}
+            label="Decision message"
+            descriptiveText="We will email this to the user"
+            placeholder="Congratulations! We have decided to accept your application for our Homeownership Program. We will send further information via email."
+            rows={3}
+            hasError={errorsDecide?.message}
+            errorMessage="Invalid message"
+          />
+          <Flex justifyContent="end" marginTop="1rem">
+            <Button variation="destructive" onClick={handleDecideModalOnClose}>
+              Cancel
+            </Button>
+            <Button type="submit">Send</Button>
+          </Flex>
+        </form>
+      </Modal>
+      <Flex justifyContent="end">
+        <Button onClick={handleReturnOnClick}>Return</Button>
+        <Button variation="primary" onClick={handleDecideOnClick}>
+          Decide
+        </Button>
+      </Flex>
     </Flex>
   );
 };
