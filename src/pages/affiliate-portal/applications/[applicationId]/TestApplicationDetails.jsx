b6e524bef1cbf8ce8f4aded12466b1d200506395
Diego Quinteros <dev.quinteros@gmail.com>
2024-01-31 12:20:17 -0600

    🐛 Fix TestApplicationDetails letting the user send another email while one is being sent

--- src/pages/affiliate-portal/applications/[applicationId]/TestApplicationDetails.jsx
+++ src/pages/affiliate-portal/applications/[applicationId]/TestApplicationDetails.jsx
@@ -129,7 +129,8 @@ const TestApplicationDetails = () => {
 
   const handleReturnOnClick = () => setReturnModalOpen(true);
 
-  const handleReturnModalOnClose = () => setReturnModalOpen(false);
+  const handleReturnModalOnClose = () =>
+    loading === 0 && setReturnModalOpen(false);
 
   const handleOnValidReturn = async (data) => {
     setLoading((previousLoading) => previousLoading + 1);
@@ -161,7 +162,8 @@ const TestApplicationDetails = () => {
 
   const handleDecideOnClick = () => setDecideModalOpen(true);
 
-  const handleDecideModalOnClose = () => setDecideModalOpen(false);
+  const handleDecideModalOnClose = () =>
+    loading === 0 && setDecideModalOpen(false);
 
   const handleOnValidDecide = async (data) => {
     setLoading((previousLoading) => previousLoading + 1);
@@ -302,10 +304,16 @@ const TestApplicationDetails = () => {
             </View>
           )}
           <Flex justifyContent="end" marginTop="1rem">
-            <Button variation="destructive" onClick={handleReturnModalOnClose}>
+            <Button
+              variation="destructive"
+              onClick={handleReturnModalOnClose}
+              isDisabled={loading > 0}
+            >
               Cancel
             </Button>
-            <Button type="submit">Return</Button>
+            <Button type="submit" isDisabled={loading > 0}>
+              Return
+            </Button>
           </Flex>
         </form>
       </Modal>
@@ -353,10 +361,16 @@ const TestApplicationDetails = () => {
             </View>
           )}
           <Flex justifyContent="end" marginTop="1rem">
-            <Button variation="destructive" onClick={handleDecideModalOnClose}>
+            <Button
+              variation="destructive"
+              onClick={handleDecideModalOnClose}
+              isDisabled={loading > 0}
+            >
               Cancel
             </Button>
-            <Button type="submit">Send</Button>
+            <Button type="submit" isDisabled={loading > 0}>
+              Send
+            </Button>
           </Flex>
         </form>
       </Modal>
