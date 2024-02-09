0606a0f6c14a296c4688b0fe68669922c03624a5
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-08 21:35:55 -0600

    ✨ Update TestLayout to use new TestApplication model

--- src/components/Test/Layout/TestLayout.jsx
+++ src/components/Test/Layout/TestLayout.jsx
@@ -63,6 +63,8 @@ export function TestLayout() {
           lastSection: location.pathname,
           members: [],
           submitted: false,
+          submissionStatus: 'Unsubmitted',
+          reviewStatus: 'Pending',
           testApplicationAffiliateId: habitat.id,
         })
       );
