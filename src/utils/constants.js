ed41884a9cd20686602fb43f676ae58b6fb3b2f3
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-13 14:20:40 -0600

    ✨ Add ETHNICITY_OPTIONS and RACE_OPTIONS constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -88,3 +88,41 @@ export const SUBMISSION_STATUS_LIST = mapEnumToList(SubmissionStatus);
  */
 export const SUBMISSION_STATUS_MAP =
   mapEnumToHumanReadableValues(SubmissionStatus);
+
+/**
+ * This constant is used to store the available ethnicity options
+ */
+export const ETHNICITY_OPTIONS = {
+  hispanicOrLatino: 'Hispanic or Latino',
+  mexican: 'Mexican',
+  puertoRican: 'Puerto Rican',
+  cuban: 'Cuban',
+  otherHispanicOrLatino: 'Other Hispanic or Latino',
+  notHispanicOrLatino: 'Not Hispanic or Latino',
+  iDoNotWishToProvideThisInfo: 'I do not wish to provide this info',
+};
+
+/**
+ * This constant is used to store the available race options
+ */
+export const RACE_OPTIONS = {
+  americanIndianOrAlaskaNative: 'American Indian or Alaska Native',
+  nameOfEnrolledOrPrincipalTribe: 'Name of enrolled or principal tribe',
+  asian: 'Asian',
+  asianIndian: 'Asian Indian',
+  chinese: 'Chinese',
+  filipino: 'Filipino',
+  japanese: 'Japanese',
+  korean: 'Korean',
+  vietnamese: 'Vietnamese',
+  otherAsian: 'Other Asian',
+  blackOrAfricanAmerican: 'Black or African American',
+  nativeHawaiianOrOtherPacificIslander:
+    'Native Hawaiian or other Pacific Islander',
+  nativeHawaiian: 'Native Hawaiian',
+  guamanianOrChamorro: 'Guamanian or Chamorro',
+  samoan: 'Samoan',
+  otherPacificIslander: 'Other Pacific Islander',
+  white: 'White',
+  iDoNotWishToProvideThisInfo: 'I do not wish to provide this information',
+};
