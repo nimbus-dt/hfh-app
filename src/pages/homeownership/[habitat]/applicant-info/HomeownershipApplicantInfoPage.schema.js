6aae4bf4a8b71bee0a2dacb51b07366c0c91b3bc
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-20 19:37:10 -0600

    🚚 Move TestApplicantInfo to pages and rename it to HomeownershipApplicantInfoPage

---
+++ src/pages/homeownership/[habitat]/applicant-info/HomeownershipApplicantInfoPage.schema.js
@@ -0,0 +1,57 @@
+import { z } from 'zod';
+
+export const maritalStatusValues = [
+  'Married',
+  'Separated',
+  'Unmarried(single, divorced, widowed, civil union, domestic partnership, registered reciprocal beneficiary relationship)',
+];
+
+export const ownerShipValues = ['Own', 'Rent'];
+
+export const unmarriedRelationshipTypesValues = [
+  'Civil union',
+  'Domestic partnership',
+  'Register reciprocal beneficiary relationship',
+  'Other',
+];
+
+const phoneSchema = z
+  .string()
+  .regex(
+    /\(\d{3}\)\s\d{3}-\d{4}/i,
+    'Invalid phone number, must be in format (000) 000-0000'
+  );
+
+const optionalPhoneSchema = z
+  .union([z.string().length(0), phoneSchema])
+  .optional()
+  .transform((phone) => (phone === '' ? undefined : phone));
+
+export const basicInfoSchema = z.object({
+  fullName: z.string().min(1),
+  altOrFormerName: z.string(),
+  socialSecurityNumber: z
+    .string()
+    .regex(
+      /\d{3}-\d{2}-\d{4}/i,
+      'Invalid social security number, must be a 9 digit number in format AAA-GG-SSSS'
+    ),
+  cellPhone: phoneSchema,
+  homePhone: optionalPhoneSchema,
+  workPhone: optionalPhoneSchema,
+  birthDate: z.string(),
+  maritalStatus: z.enum(maritalStatusValues),
+});
+
+export const addressSchema = z.object({
+  address: z.string().min(1),
+  ownershipStatus: z.enum(ownerShipValues),
+  monthsLivedHere: z.coerce.number().int().positive(),
+});
+
+export const unmarriedAddendumSchema = z.object({
+  notSpouseButSimilarPropertyRights: z.enum(['Yes', 'No']),
+  relationshipType: z.enum(unmarriedRelationshipTypesValues).optional(),
+  otherRelationshipType: z.string().min(0).optional(),
+  state: z.string().min(0).optional(),
+});
