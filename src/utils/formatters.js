8b914736c3724e617e5fecde27bce34463522500
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-22 17:09:19 -0600

    🔥 Remove references to old Amplify models and functions

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,10 +1,3 @@
-import {
-  APPLICATION_SUBMITTED_STATUS_MAP,
-  DEBT_TYPES_MAP,
-  RELATIONSHIP_VALUES_MAP,
-  SEX_VALUES_MAP,
-} from './constants';
-
 /**
  * Returns a string with all the clases joined by empty space
  * @param {...string} classes
@@ -12,32 +5,6 @@ import {
  */
 export const concatClassNames = (...classes) =>
   classes.filter(Boolean).join(' ');
-
-/**
- * Returns a string with the sex value as a human readable string
- * @param { SexTypes } sexType
- * @returns {string}
- */
-export const formatSexValue = (sexType) => SEX_VALUES_MAP[sexType] ?? '';
-
-/**
- * Returns a string with the relationship value as a human readable string
- * @param { RelationshipTypes } relationshipType
- * @returns {string}
- */
-export const formatRelationshipValue = (relationshipType) =>
-  RELATIONSHIP_VALUES_MAP[relationshipType] ?? '';
-
-/**
- * Returns a string with the application submitted status value as a human
- * readable string
- * @param { ApplicationSubmittedStatus } type
- * @returns {string}
- */
-export const formatSubmittedStatus = (type) =>
-  APPLICATION_SUBMITTED_STATUS_MAP[type] ?? '';
-
-export const formatDebtType = (type) => DEBT_TYPES_MAP[type] ?? '';
 
 /**
  * Returns a string with the application submitted status value as a human
