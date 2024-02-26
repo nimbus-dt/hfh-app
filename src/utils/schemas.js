65f19095c8cf613a4f29f41417b4e6053e541e85
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-26 11:22:10 -0600

    ✨ Add zipCodeSchema to schemas

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -4,3 +4,18 @@ export const searchableOptionSchema = z.object({
   id: z.union([z.string().min(1), z.number()]),
   label: z.string(),
 });
+
+export const zipCodeSchema = z.union([
+  z
+    .string()
+    .regex(
+      /\b\d{5}\b/g,
+      'Invalid Zip Code should be in the format 12345 or 12345-6789'
+    ),
+  z
+    .string()
+    .regex(
+      /\b\d{5}-\d{4}\b/g,
+      'Invalid Zip Code should be in the format 12345 or 12345-6789'
+    ),
+]);
