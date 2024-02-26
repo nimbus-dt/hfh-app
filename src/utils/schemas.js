2b2d24bfe4d989b241f42b18256871fbf311df45
Diego Quinteros <dev.quinteros@gmail.com>
2024-02-26 13:26:04 -0600

    🐛 Fix zipCodeSchema accepting wrong zip format

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -5,15 +5,19 @@ export const searchableOptionSchema = z.object({
   label: z.string(),
 });
 
+const invalidZipCodeMessage =
+  'Invalid Zip Code should be in the format 12345 or 12345-6789';
+
 export const zipCodeSchema = z.union([
   z
     .string()
-    .regex(
-      /\b\d{5}\b/g,
-      'Invalid Zip Code should be in the format 12345 or 12345-6789'
-    ),
+    .min(5, invalidZipCodeMessage)
+    .max(5, invalidZipCodeMessage)
+    .regex(/\b\d{5}\b/g, invalidZipCodeMessage),
   z
     .string()
+    .min(10, invalidZipCodeMessage)
+    .max(10, invalidZipCodeMessage)
     .regex(
       /\b\d{5}-\d{4}\b/g,
       'Invalid Zip Code should be in the format 12345 or 12345-6789'
