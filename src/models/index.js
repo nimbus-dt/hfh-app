2b65101a51de293ebe2fb0ee87cb9b6cbe39c80a
Diego Quinteros <dev.quinteros@gmail.com>
2024-03-05 09:18:01 -0600

    👽️ Update Amplify files due to changes on habitat data model

--- src/models/index.js
+++ src/models/index.js
@@ -13,7 +13,7 @@ const SubmissionStatus = {
   "RETURNED": "RETURNED"
 };
 
-const { Property, TestCycle, ApplicantOptional, Asset, Debt, Income, TestApplication, EmploymentInfo, Member, Record, Written, Checklist, ApplicantInfo, Habitat, WrittenQuestion, RecordQuestion, CheckQuestion, TextSection, HabitatProps } = initSchema(schema);
+const { Property, TestCycle, ApplicantOptional, Asset, Debt, Income, TestApplication, EmploymentInfo, Member, Record, Written, Checklist, ApplicantInfo, Habitat, OptionalSections, WrittenQuestion, RecordQuestion, CheckQuestion, TextSection, HabitatProps } = initSchema(schema);
 
 export {
   Property,
@@ -32,6 +32,7 @@ export {
   Habitat,
   ApplicationTypes,
   SubmissionStatus,
+  OptionalSections,
   WrittenQuestion,
   RecordQuestion,
   CheckQuestion,
