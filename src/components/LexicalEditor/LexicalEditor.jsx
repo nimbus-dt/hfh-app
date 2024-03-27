9b1763b174158f7bf4f14f3c9542869a95fa603e
Diego Quinteros <dev.quinteros@gmail.com>
2024-03-27 16:24:18 -0600

    ✨ Add LexicalEditor Component

---
+++ src/components/LexicalEditor/LexicalEditor.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { Text, View } from '@aws-amplify/ui-react';
+import PropTypes from 'prop-types';
+import ToolbarPlugin from './components/ToolbarPlugin';
+import LexicalEditorTheme from './LexicalEditor.Theme';
+import './LexicalEditor.style.css';
+
+function Placeholder() {
+  return (
+    <Text
+      opacity="50"
+      position="absolute"
+      top="0.75rem"
+      left="0.5rem"
+      overflow="hidden"
+      style={{
+        textOverflow: 'ellipsis',
+        pointerEvents: 'none',
+      }}
+    >
+      Enter some text...
+    </Text>
+  );
+}
+
+const LexicalEditor = ({ editorState, onChange }) => (
+  <LexicalComposer
+    initialConfig={{
+      editorState,
+      theme: LexicalEditorTheme,
+      namespace: 'MyEditor',
+      onError: (error) => console.log('Lexical error', error),
+    }}
+  >
+    <View
+      border="var(--amplify-colors-border-primary)"
+      borderStyle="solid"
+      borderRadius="small"
+      borderWidth="medium"
+    >
+      <ToolbarPlugin />
+      <View position="relative">
+        <RichTextPlugin
+          contentEditable={<ContentEditable className="editor-input" />}
+          ErrorBoundary={LexicalErrorBoundary}
+          placeholder={<Placeholder />}
+        />
+        <HistoryPlugin />
+        <AutoFocusPlugin />
+        <OnChangePlugin onChange={onChange} />
+      </View>
+    </View>
+  </LexicalComposer>
+);
+
+LexicalEditor.propTypes = {
+  editorState: PropTypes.object,
+  onChange: PropTypes.func,
+};
+
+export default LexicalEditor;
---
+++ src/components/LexicalEditor/components/ToolbarPlugin.jsx
@@ -0,0 +1,203 @@
+import { Button, Divider, Flex, View } from '@aws-amplify/ui-react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { mergeRegister } from '@lexical/utils';
+import {
+  $getSelection,
+  $isRangeSelection,
+  CAN_REDO_COMMAND,
+  CAN_UNDO_COMMAND,
+  FORMAT_ELEMENT_COMMAND,
+  FORMAT_TEXT_COMMAND,
+  REDO_COMMAND,
+  SELECTION_CHANGE_COMMAND,
+  UNDO_COMMAND,
+} from 'lexical';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import * as React from 'react';
+import {
+  MdFormatAlignCenter,
+  MdFormatAlignJustify,
+  MdFormatAlignLeft,
+  MdFormatAlignRight,
+  MdFormatBold,
+  MdFormatItalic,
+  MdFormatStrikethrough,
+  MdFormatUnderlined,
+  MdRedo,
+  MdUndo,
+} from 'react-icons/md';
+
+const LowPriority = 1;
+
+const buttonProps = {
+  padding: '.5rem',
+  borderStyle: 'none',
+};
+
+const VerticalDivider = () => <Divider orientation="vertical" opacity={1} />;
+
+const ToolbarPlugin = () => {
+  const [editor] = useLexicalComposerContext();
+  const toolbarRef = useRef(null);
+  const [canUndo, setCanUndo] = useState(false);
+  const [canRedo, setCanRedo] = useState(false);
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isUnderline, setIsUnderline] = useState(false);
+  const [isStrikethrough, setIsStrikethrough] = useState(false);
+
+  const updateToolbar = useCallback(() => {
+    const selection = $getSelection();
+    if ($isRangeSelection(selection)) {
+      // Update text format
+      setIsBold(selection.hasFormat('bold'));
+      setIsItalic(selection.hasFormat('italic'));
+      setIsUnderline(selection.hasFormat('underline'));
+      setIsStrikethrough(selection.hasFormat('strikethrough'));
+    }
+  }, []);
+
+  useEffect(
+    () =>
+      mergeRegister(
+        editor.registerUpdateListener(({ editorState }) => {
+          editorState.read(() => {
+            updateToolbar();
+          });
+        }),
+        editor.registerCommand(
+          SELECTION_CHANGE_COMMAND,
+          (_payload) => {
+            updateToolbar();
+            return false;
+          },
+          LowPriority
+        ),
+        editor.registerCommand(
+          CAN_UNDO_COMMAND,
+          (payload) => {
+            setCanUndo(payload);
+            return false;
+          },
+          LowPriority
+        ),
+        editor.registerCommand(
+          CAN_REDO_COMMAND,
+          (payload) => {
+            setCanRedo(payload);
+            return false;
+          },
+          LowPriority
+        )
+      ),
+    [editor, updateToolbar]
+  );
+
+  return (
+    <View position="sticky" top="0rem">
+      <Flex gap="0.25rem" ref={toolbarRef} padding="0.25rem">
+        <Button
+          isDisabled={!canUndo}
+          onClick={() => {
+            editor.dispatchCommand(UNDO_COMMAND, undefined);
+          }}
+          aria-label="Undo"
+          {...buttonProps}
+        >
+          <MdUndo />
+        </Button>
+        <Button
+          disabled={!canRedo}
+          onClick={() => {
+            editor.dispatchCommand(REDO_COMMAND, undefined);
+          }}
+          aria-label="Redo"
+          {...buttonProps}
+        >
+          <MdRedo />
+        </Button>
+        <VerticalDivider />
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold');
+          }}
+          aria-label="Format Bold"
+          {...buttonProps}
+          backgroundColor={isBold && 'brand.primary.10'}
+        >
+          <MdFormatBold />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic');
+          }}
+          {...buttonProps}
+          backgroundColor={isItalic && 'brand.primary.10'}
+          aria-label="Format Italics"
+        >
+          <MdFormatItalic />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline');
+          }}
+          {...buttonProps}
+          backgroundColor={isUnderline && 'brand.primary.10'}
+          aria-label="Format Underline"
+        >
+          <MdFormatUnderlined />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough');
+          }}
+          {...buttonProps}
+          backgroundColor={isStrikethrough && 'brand.primary.10'}
+          aria-label="Format Strikethrough"
+        >
+          <MdFormatStrikethrough />
+        </Button>
+        <VerticalDivider />
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'left');
+          }}
+          {...buttonProps}
+          aria-label="Left Align"
+        >
+          <MdFormatAlignLeft />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'center');
+          }}
+          {...buttonProps}
+          aria-label="Center Align"
+        >
+          <MdFormatAlignCenter />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'right');
+          }}
+          {...buttonProps}
+          aria-label="Right Align"
+        >
+          <MdFormatAlignRight />
+        </Button>
+        <Button
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'justify');
+          }}
+          {...buttonProps}
+          aria-label="Justify Align"
+        >
+          <MdFormatAlignJustify />
+        </Button>{' '}
+      </Flex>
+      <Divider opacity={1} />
+    </View>
+  );
+};
+
+export default ToolbarPlugin;
