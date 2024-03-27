24213545acd3b0600726d6ed729b28589aba69a7
Diego Quinteros <dev.quinteros@gmail.com>
2024-03-27 16:24:59 -0600

    ✨ Add NoteModal to ApplicationDetailsPage components

---
+++ src/pages/affiliate-portal/cycles/[cycleId]/[applicationId]/components/NoteModal/NoteModal.jsx
@@ -0,0 +1,35 @@
+import LexicalEditor from 'components/LexicalEditor';
+import Modal from 'components/Modal';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Flex } from '@aws-amplify/ui-react';
+
+const NoteModal = ({ open, onClose, onSave }) => {
+  const [editorState, setEditorState] = useState();
+
+  const handleOnChange = (state) => {
+    setEditorState(state.toJSON());
+  };
+
+  const handleOnSave = () => {
+    onSave(editorState);
+    setEditorState();
+  };
+
+  return (
+    <Modal title="Note" open={open} onClickClose={onClose} width="45rem">
+      <LexicalEditor editorState={editorState} onChange={handleOnChange} />
+      <Flex justifyContent="right">
+        <Button onClick={handleOnSave}>Save</Button>
+      </Flex>
+    </Modal>
+  );
+};
+
+NoteModal.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onSave: PropTypes.func,
+};
+
+export default NoteModal;
