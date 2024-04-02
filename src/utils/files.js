87c5b146ceec91ba4dd62e4f2afdac8e6d3b3bd7
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-02 11:46:55 -0600

    ✨ Add removeFiles and fileFromObjectURL util functions

--- src/utils/files.js
+++ src/utils/files.js
@@ -1,3 +1,5 @@
+import { Storage } from 'aws-amplify';
+
 /**
  * Downloads a file from a valid URL
  * @param {string} url
@@ -9,4 +11,38 @@ export const downloadWithUrl = (url) => {
   a.click();
 
   a.remove();
+};
+
+/**
+ * Remove files from S3 bucket
+ * @param {string[]} keys
+ * @param {string} level
+ * @returns {Array}
+ */
+export const removeFiles = async (keys, level = 'public') => {
+  const promisesArr = keys.map((key) =>
+    Storage.remove(key, {
+      level,
+    })
+  );
+  const results = await Promise.all(promisesArr);
+  return results;
+};
+
+/**
+ * Convert object url to file
+ * @param {string} objectUrl
+ * @param {string} name
+ * @returns {File}
+ */
+
+export const fileFromObjectURL = async (objectUrl, name) => {
+  const file = await fetch(objectUrl)
+    .then((r) => r.blob())
+    .then(
+      (blobFile) =>
+        new File([blobFile], name, { type: `image/${name.split('.').pop()}` })
+    );
+
+  return file;
 };
