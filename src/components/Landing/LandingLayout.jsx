18ade764969bec653282edfdd21df33207c2b3a5
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-04 15:07:00 -0600

    ⚰️ Remove unused code

--- src/components/Landing/LandingLayout.jsx
+++ src/components/Landing/LandingLayout.jsx
@@ -1,17 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
-import { Flex, Button } from '@aws-amplify/ui-react';
+import { Flex } from '@aws-amplify/ui-react';
 import { LandingFooter } from './LandingFooter';
 
 export function LandingLayout({ comp }) {
-  function handleScrollToContactForm() {
-    const contactFormSection = document.getElementById('contactFormSection');
-    if (contactFormSection) {
-      contactFormSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-
   return (
     <Flex width="100%" direction="column" gap="0" height="fit-content">
       <Flex direction="column" width="100%" height="fit-content">
