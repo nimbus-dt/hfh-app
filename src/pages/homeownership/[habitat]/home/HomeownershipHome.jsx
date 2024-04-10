4f8e091a5665efe3a9b0221dc91a4be971fe743c
batarse1 <00014419@uca.edu.sv>
2024-04-10 09:40:51 -0600

    ✨ Add Form to HomeownershipHome and add customComponents

--- src/pages/homeownership/[habitat]/home/HomeownershipHome.jsx
+++ src/pages/homeownership/[habitat]/home/HomeownershipHome.jsx
@@ -1,98 +1,32 @@
-import {
-  View,
-  Flex,
-  Text,
-  Heading,
-  Button,
-  Authenticator,
-  ThemeProvider,
-  useBreakpointValue,
-} from '@aws-amplify/ui-react';
-import { Link, useNavigate, useOutletContext } from 'react-router-dom';
-import { SubmissionStatus } from 'models';
+import { useOutletContext } from 'react-router-dom';
+import { Form } from '@formio/react';
 import CustomCard from 'components/CustomCard';
-import DOMPurify from 'dompurify';
+import 'utils/customComponents';
 
-export default function HomeownershipHomePage() {
+const FORMIO_URL = process.env.REACT_APP_FORMIO_URL;
+
+const HomeownershipHomePage = () => {
   const { application, habitat, openCycle } = useOutletContext();
-  const navigate = useNavigate();
 
-  const isReallySmall = useBreakpointValue({
-    base: true,
-    small: false,
-  });
-
-  const content = () => {
-    if (
-      application &&
-      (openCycle || application.submissionStatus === SubmissionStatus.RETURNED)
-    ) {
-      return application.submissionStatus === SubmissionStatus.SUBMITTED ? (
-        <>
-          <Text fontWeight="bold">
-            {`You have succesfully submitted your Homeownership Program application
-          for ${habitat?.name}. You will receive an email with updates on your
-          application.`}
-          </Text>
-          <Flex justifyContent="end">
-            <Link to="../review">
-              <Button variation="primary">Review</Button>
-            </Link>
-          </Flex>
-        </>
-      ) : (
-        <>
-          <View as="div">
-            <Flex marginBottom="30px" direction="column" gap="xl">
-              <View
-                as="div"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(
-                    habitat?.props.homeownershipHomeText
-                  ),
-                }}
-              />
-            </Flex>
-          </View>
-          <Flex justifyContent="end" width="100%">
-            <Button variation="primary" onClick={() => navigate('../terms')}>
-              Next
-            </Button>
-          </Flex>
-        </>
-      );
-    }
-
-    return (
-      <Text fontWeight="bold">{habitat?.props.homeownershipNoOpenCycle}</Text>
-    );
-  };
+  if (!habitat || !openCycle) {
+    return <p>loading...</p>;
+  }
 
   return (
     <CustomCard>
-      <ThemeProvider
-        theme={{
-          name: 'homeownership-authentication',
-          tokens: {
-            components: {
-              authenticator: {
-                container: {
-                  widthMax: '100%',
-                },
-                router: {
-                  borderStyle: 'none',
-                  boxShadow: 'none',
-                },
-                form: {
-                  padding: isReallySmall ? '0.25rem' : undefined,
-                },
-              },
-            },
+      <Form
+        src={`${FORMIO_URL}/loudoun`}
+        onSubmit={console.log}
+        options={{
+          additional: {
+            application,
+            habitat,
+            openCycle,
           },
         }}
-      >
-        <Authenticator>{content}</Authenticator>
-      </ThemeProvider>
+      />
     </CustomCard>
   );
-}
+};
+
+export default HomeownershipHomePage;
