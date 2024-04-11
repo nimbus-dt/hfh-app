a1e826d1c6857bbf3a81c0dc6fb7ce0e1eb2dbc2
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-11 14:02:23 -0600

    ✨ Update SaveButton to be able to update existing FormAnswers

--- src/components/Formio/SaveButton/SaveButton.js
+++ src/components/Formio/SaveButton/SaveButton.js
@@ -4,16 +4,36 @@ import { DataStore } from 'aws-amplify';
 import { FormAnswer } from 'models';
 
 const saveSection = async ({ data, application, page, section }) => {
-  const response = await DataStore.save(
-    new FormAnswer({
-      testapplicationID: application.id,
-      page,
-      section,
-      values: data,
+  const persistedFormAnswer = await DataStore.query(FormAnswer, (c1) =>
+    c1.and((c2) => {
+      const criteriaArray = [
+        c2.testapplicationID.eq(application.id),
+        c2.page.eq(page),
+        c2.section.eq(section),
+      ];
+
+      return criteriaArray;
     })
   );
 
-  console.log(response);
+  if (persistedFormAnswer.length > 0) {
+    const response = await DataStore.save(
+      FormAnswer.copyOf(persistedFormAnswer[0], (original) => {
+        original.values = data;
+      })
+    );
+    console.log('update response', response);
+  } else {
+    const response = await DataStore.save(
+      new FormAnswer({
+        testapplicationID: application.id,
+        page,
+        section,
+        values: data,
+      })
+    );
+    console.log('save response', response);
+  }
 };
 
 class SaveButton extends Components.components.button {
