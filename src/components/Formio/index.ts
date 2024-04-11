20ae64a065dd9b94a89d499305f27b53e833db35
batarse1 <00014419@uca.edu.sv>
2024-04-11 15:29:00 -0600

    ✨ Add City component to Formio components

--- src/components/Formio/index.ts
+++ src/components/Formio/index.ts
@@ -2,7 +2,9 @@
 import { Components } from 'formiojs';
 
 import SaveButton from './SaveButton';
+import City from './City';
 
 Components.setComponents({
   saveButton: SaveButton,
+  city: City
 });
