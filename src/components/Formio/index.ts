48368dabe52c755822b9ff39cde9e4c22ac873ca
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-12 13:58:04 -0600

    ✨ Update formio components to replace container with CustomContainer

--- src/components/Formio/index.ts
+++ src/components/Formio/index.ts
@@ -1,10 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { Components } from 'formiojs';
 
 import SaveButton from './SaveButton';
 import City from './City';
+import CustomContainer from './CustomContainer';
 
 Components.setComponents({
   saveButton: SaveButton,
-  city: City
+  city: City,
+  container: CustomContainer,
 });
