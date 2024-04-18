35b723792fe2ba8f8f94e69c05bb884f1d0c0ee9
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-18 11:07:50 -0600

    ✨ Update formio components to not replace container with CustomContainer

--- src/components/Formio/index.ts
+++ src/components/Formio/index.ts
@@ -2,12 +2,10 @@ import { Components } from 'formiojs';
 
 import SaveButton from './SaveButton';
 import City from './City';
-import CustomContainer from './CustomContainer';
 import Review from './Review';
 
 Components.setComponents({
   saveButton: SaveButton,
   city: City,
-  container: CustomContainer,
   review: Review,
 });
