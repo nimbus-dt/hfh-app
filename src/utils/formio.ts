b4082859ce98771bcbc1a3bd02d7eea4187243eb
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-18 13:51:24 -0600

    ✨ Update generateSubmission to only use page and not section of formAnswer

--- src/utils/formio.ts
+++ src/utils/formio.ts
@@ -3,23 +3,22 @@ import { cloneDeep } from 'lodash';
 
 interface ISubmission {
   data: {
-    [key: string]: {
-      [key: string]: unknown;
-    };
+    [key: string]: unknown;
   };
 }
 
 export const generateSubmission = (formAnswers: FormAnswer[]): ISubmission => {
   const submissionData: ISubmission['data'] = {};
+
   for (const formAnswer of formAnswers) {
-    if (formAnswer.page && formAnswer.section) {
-      submissionData[formAnswer.page] = {
-        ...submissionData[formAnswer.page],
-        [formAnswer.section]: cloneDeep(formAnswer.values),
-      };
+    if (formAnswer.page && formAnswer.values) {
+      submissionData[formAnswer.page] = cloneDeep(formAnswer.values);
     }
   }
-  return {
+
+  const submission = {
     data: submissionData,
   };
+
+  return submission;
 };
