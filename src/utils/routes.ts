eb3bf1074b3370d99c9339b8abb18e6c9e6b24aa
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-24 13:32:21 -0600

    ✨ Add ROUTES constant and getRouteTitle util function

---
+++ src/utils/routes.ts
@@ -0,0 +1,27 @@
+/**
+ * This constant is used to store the routes and their titles
+ * Things to consider: dynamic parths should start with a colon,
+ * this way the getRouteTitle function can replace the dynamic part with the route part
+ */
+
+export const ROUTES = {
+  applicantApplications: {
+    title: 'Applications',
+    route: '/:habitat/applicant/applications',
+  },
+};
+
+export const getRouteTitle = (route: string) => {
+  const title = Object.entries(ROUTES).find(([, value]) => {
+    const routeArray = value.route.split('/');
+    const realRouteArray = route.split('/');
+    for (let i = 0; i < routeArray.length; i += 1) {
+      if (routeArray[i].startsWith(':')) {
+        realRouteArray[i] = routeArray[i];
+      }
+    }
+    return routeArray.join('/') === realRouteArray.join('/');
+  });
+
+  return title ? title[1].title : undefined;
+};
