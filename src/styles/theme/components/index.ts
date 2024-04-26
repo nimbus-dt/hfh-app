00fffa7146b072713cebbb6f43824446a383106e
batarse1 <00014419@uca.edu.sv>
2024-04-26 13:13:49 -0600

    ✨ Update theme

---
+++ src/styles/theme/components/index.ts
@@ -0,0 +1,15 @@
+import authenticator from './authenticator';
+import button from './button';
+import input from './input';
+import field from './field';
+import fieldcontrol from './fieldcontrol';
+
+const components = {
+  authenticator,
+  button,
+  input,
+  field,
+  fieldcontrol,
+};
+
+export default components;
