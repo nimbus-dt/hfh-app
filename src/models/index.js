768b6e9193234afe25cef26fb6038a592f8ebbf4
Diego Quinteros <dev.quinteros@gmail.com>
2024-04-30 11:16:53 -0600

    👽️ Update Amplify files due to changes to the data models

--- src/models/index.js
+++ src/models/index.js
@@ -2,20 +2,27 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
+const RootFormStatusTypes = {
+  "ACTIVE": "ACTIVE",
+  "PENDING": "PENDING"
+};
+
 const ApplicationTypes = {
   "ONLINE": "ONLINE",
   "PAPER": "PAPER"
 };
 
 const SubmissionStatus = {
-  "SUBMITTED": "SUBMITTED",
-  "UNSUBMITTED": "UNSUBMITTED",
+  "PENDING": "PENDING",
+  "ACCEPTED": "ACCEPTED",
+  "REJECTED": "REJECTED",
   "RETURNED": "RETURNED"
 };
 
-const { Form, Decision, FormAnswer, Note, Property, TestCycle, ApplicantOptional, Asset, Debt, Income, TestApplication, EmploymentInfo, Member, Record, Written, Checklist, ApplicantInfo, Habitat, GalleryItem, OptionalSections, WrittenQuestion, RecordQuestion, CheckQuestion, HabitatProps } = initSchema(schema);
+const { RootForm, Form, Decision, FormAnswer, Note, Property, TestCycle, ApplicantOptional, Asset, Debt, Income, TestApplication, EmploymentInfo, Member, Record, Written, Checklist, ApplicantInfo, Habitat, GalleryItem, OptionalSections, WrittenQuestion, RecordQuestion, CheckQuestion, HabitatProps } = initSchema(schema);
 
 export {
+  RootForm,
   Form,
   Decision,
   FormAnswer,
@@ -34,6 +41,7 @@ export {
   Checklist,
   ApplicantInfo,
   Habitat,
+  RootFormStatusTypes,
   ApplicationTypes,
   SubmissionStatus,
   GalleryItem,
