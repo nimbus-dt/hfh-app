ffa7824a559d9b383fc449cbd889a9cf6a6ef52e
Diego Quinteros <dev.quinteros@gmail.com>
2024-05-02 15:41:46 -0600

    ✨ Update ApplicantApplicationsPage to retrieve data from the backend

--- src/pages/[habitat]/applicant/applications/ApplicantApplicationsPage.tsx
+++ src/pages/[habitat]/applicant/applications/ApplicantApplicationsPage.tsx
@@ -1,104 +1,83 @@
-import { Button, Flex, Heading, Text, View } from '@aws-amplify/ui-react';
-import { SubmissionStatus } from 'models';
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  useAuthenticator,
+  View,
+} from '@aws-amplify/ui-react';
+import {
+  LazyRootForm,
+  LazyTestApplication,
+  LazyTestCycle,
+  ReviewStatus,
+  RootForm,
+  SubmissionStatus,
+  TestApplication,
+  TestCycle,
+} from 'models';
 import React, { useState } from 'react';
 import { MdOutlineOpenInNew } from 'react-icons/md';
 import TableWithPaginator from 'components/TableWithPaginator';
-import Chip from 'components/Chip';
-import { stringToHumanReadable } from 'utils/strings';
 import Toggle from 'components/Toggle';
 import StatusChip from 'components/StatusChip';
+import {
+  useRootFormsQuery,
+  useTestApplicationsQuery,
+  useTestCyclesQuery,
+} from 'hooks/services';
+import {
+  RecursiveModelPredicate,
+  SortDirection,
+  SortPredicate,
+} from '@aws-amplify/datastore';
+import { Link } from 'react-router-dom';
+import { dateOnly } from 'utils/dates';
+import Chip from 'components/Chip';
+import { stringToHumanReadable } from 'utils/strings';
 import style from './ApplicantApplicationsPage.module.css';
 
-const dummyData = [
-  {
-    name: 'Homeownership',
-    affiliate: 'Habitat for Humanity of Kenosha',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.PENDING,
-  },
-  {
-    name: 'Repairs',
-    affiliate: 'Habitat for Humanity',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.REJECTED,
-  },
-  {
-    name: 'Pre-Screen',
-    affiliate: 'Habitat for Humanity of Loudon County',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.RETURNED,
-  },
-  {
-    name: 'Homeownership',
-    affiliate: 'Habitat for Humanity of Kenosha',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.PENDING,
-  },
-  {
-    name: 'Repairs',
-    affiliate: 'Habitat for Humanity',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.REJECTED,
-  },
-  {
-    name: 'Pre-Screen',
-    affiliate: 'Habitat for Humanity of Loudon County',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.RETURNED,
-  },
-  {
-    name: 'Homeownership',
-    affiliate: 'Habitat for Humanity of Kenosha',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.PENDING,
-  },
-  {
-    name: 'Repairs',
-    affiliate: 'Habitat for Humanity',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.REJECTED,
-  },
-  {
-    name: 'Pre-Screen',
-    affiliate: 'Habitat for Humanity of Loudon County',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.RETURNED,
-  },
-  {
-    name: 'Homeownership',
-    affiliate: 'Habitat for Humanity of Kenosha',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.PENDING,
-  },
-  {
-    name: 'Repairs',
-    affiliate: 'Habitat for Humanity',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.REJECTED,
-  },
-  {
-    name: 'Pre-Screen',
-    affiliate: 'Habitat for Humanity of Loudon County',
-    type: 'Online',
-    dateSubmitted: '04/20/2024',
-    status: SubmissionStatus.RETURNED,
-  },
-];
+const ReviewStatusChip = ({ status }: { status: keyof typeof ReviewStatus }) =>
+  status === ReviewStatus.PENDING ? (
+    <StatusChip status={status} />
+  ) : (
+    <Chip text="Reviewed" variation="active" />
+  );
 
 const ApplicantApplicationsPage = () => {
-  const [view, setView] = useState<'current' | 'past'>('current');
-  const [applications, setApplications] = useState(dummyData);
+  const [submissionStatusFilter, setSubmissionStatusFilter] = useState<
+    keyof typeof SubmissionStatus
+  >(SubmissionStatus.INCOMPLETE);
+
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  const { data: applications }: { data: TestApplication[] } =
+    useTestApplicationsQuery({
+      criteria: (c2: RecursiveModelPredicate<LazyTestApplication>) =>
+        c2.ownerID.eq(user?.username),
+      dependencyArray: [user, submissionStatusFilter],
+      paginationProducer: (s: SortPredicate<LazyTestApplication>) =>
+        s.createdAt(SortDirection.DESCENDING),
+    });
+
+  const { data: cycles }: { data: TestCycle[] } = useTestCyclesQuery({
+    criteria: (c2: RecursiveModelPredicate<LazyTestCycle>) =>
+      c2.or((c3) =>
+        applications.map((application) => c3.id.eq(application.testcycleID))
+      ),
+    dependencyArray: [applications],
+    paginationProducer: (s: SortPredicate<LazyTestCycle>) =>
+      s.createdAt(SortDirection.DESCENDING),
+  });
+
+  const { data: rootForms }: { data: RootForm[] } = useRootFormsQuery({
+    criteria: (c2: RecursiveModelPredicate<LazyRootForm>) =>
+      c2.or((c3) => cycles.map((cycle) => c3.id.eq(cycle.rootformID || ''))),
+    dependencyArray: [cycles],
+    paginationProducer: (s: SortPredicate<LazyRootForm>) =>
+      s.createdAt(SortDirection.DESCENDING),
+  });
+
   return (
     <Flex padding="32px" direction="column">
       <Flex
@@ -117,13 +96,14 @@ const ApplicantApplicationsPage = () => {
         </Flex>
         <Flex className={`${style.toggleContainer}`}>
           <Toggle
-            option1={{ value: 'current', label: 'Current' }}
-            option2={{ value: 'past', label: 'Past' }}
-            active={view}
+            option1={{
+              value: SubmissionStatus.INCOMPLETE,
+              label: 'Incomplete',
+            }}
+            option2={{ value: SubmissionStatus.COMPLETED, label: 'Complete' }}
+            active={submissionStatusFilter}
             onChange={(newValue) => {
-              if (newValue === 'current' || newValue === 'past') {
-                setView(newValue);
-              }
+              setSubmissionStatusFilter(newValue);
             }}
           />
         </Flex>
@@ -137,17 +117,13 @@ const ApplicantApplicationsPage = () => {
             id: 'name',
             value: 'Name',
           },
+
           {
-            id: 'affiliate',
-            value: 'Affiliate',
-          },
-          {
-            id: 'type',
-            value: 'Type',
-          },
-          {
-            id: 'dateSubmitted',
-            value: 'Date submitted',
+            id: 'date',
+            value:
+              submissionStatusFilter === SubmissionStatus.INCOMPLETE
+                ? 'Date Started'
+                : 'Date Completed',
           },
           {
             id: 'status',
@@ -160,43 +136,68 @@ const ApplicantApplicationsPage = () => {
             textAlign: 'center',
           },
         ]}
-        data={applications.map((data, index) => ({
-          id: index,
-          cells: [
-            { value: data.name, id: 'name' },
-            { value: data.affiliate, id: 'affiliate' },
-            { value: data.type, id: 'type' },
-            { value: data.dateSubmitted, id: 'dateSubmitted' },
-            {
-              value: (
-                <Flex width="100%" justifyContent="center">
-                  <StatusChip status={data.status} />
-                </Flex>
-              ),
-              id: 'status',
-            },
-            {
-              value: (
-                <Flex width="100%" justifyContent="center">
-                  <Button variation="link" padding="0">
-                    <MdOutlineOpenInNew
-                      size="24px"
-                      color="var(--amplify-colors-neutral-90)"
-                    />
-                  </Button>
-                </Flex>
-              ),
-              id: 'view',
-            },
-          ],
-        }))}
-        hasMoreData
-        loadMoreData={() =>
-          setApplications((prevApplications) => [
-            ...prevApplications,
-            ...dummyData,
-          ])
-        }
+        data={applications
+          .filter(
+            (application) =>
+              application.submissionStatus === submissionStatusFilter
+          )
+          .map((application, index) => ({
+            id: index,
+            cells: [
+              {
+                value:
+                  rootForms.find((rootForm) => {
+                    const foundCycle = cycles.find(
+                      (cycle) => cycle.id === application.testcycleID
+                    );
+                    return foundCycle && rootForm.id === foundCycle.rootformID;
+                  })?.name || 'Unknown',
+                id: 'name',
+              },
+              {
+                value: dateOnly(
+                  submissionStatusFilter === SubmissionStatus.INCOMPLETE
+                    ? application.createdAt || ''
+                    : application.submittedDate
+                ),
+                id: 'date',
+              },
+              {
+                value: (
+                  <Flex width="100%" justifyContent="center">
+                    {submissionStatusFilter === SubmissionStatus.INCOMPLETE ? (
+                      <Chip
+                        text={stringToHumanReadable(
+                          application.submissionStatus
+                        )}
+                        variation="danger"
+                      />
+                    ) : (
+                      <ReviewStatusChip status={application.reviewStatus} />
+                    )}
+                  </Flex>
+                ),
+                id: 'status',
+              },
+              {
+                value: (
+                  <Flex width="100%" justifyContent="center">
+                    <Link
+                      to={`../../${application.testcycleID}/${application.id}`}
+                    >
+                      <Button variation="link" padding="0">
+                        <MdOutlineOpenInNew
+                          size="24px"
+                          color="var(--amplify-colors-neutral-90)"
+                        />
+                      </Button>
+                    </Link>
+                  </Flex>
+                ),
+                id: 'view',
+              },
+            ],
+          }))}
       />
     </Flex>
   );
