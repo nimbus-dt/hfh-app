0dc2b465f88fbd588abf9121c3ed94d23691ff4e
Diego Quinteros <dev.quinteros@gmail.com>
2024-05-07 12:49:24 -0600

    ✨ Update convertOldRecords to update TestCycle and RootForm records

--- amplify/backend/function/convertOldRecords/src/index.js
+++ amplify/backend/function/convertOldRecords/src/index.js
@@ -16,10 +16,13 @@ const GRAPHQL_API_KEY = process.env.API_HFHAPP_GRAPHQLAPIKEYOUTPUT;
 const convertTestCycles = async () => {
     const getTestCyclesQuery = `
         query GetTestCyclesQuery {
-            listTestCycles(filter: {closedCycleMessage: {attributeExists: false}}) {
+            listTestCycles(
+                filter: {formUrl: {attributeExists: false}}
+            ) {
                 items {
                     id
                     closedCycleMessage
+                    formUrl
                 }
             }
         }
@@ -45,9 +48,10 @@ const convertTestCycles = async () => {
     for (const testCycle of testCycles) {
         const updateTestCycleQuery = `
             mutation UpdateTestCycleMutation {
-                updateTestCycle(input: {id: "${testCycle.id}", closedCycleMessage: "Cycle closed."}) {
+                updateTestCycle(input: {id: "${testCycle.id}", formUrl: "loudoun"}) {
                     id
                     closedCycleMessage
+                    formUrl
                 }
             }
         
@@ -72,10 +76,71 @@ const convertTestCycles = async () => {
     }
 }
 
+const convertRootForms = async () => {
+    const getRootFormsQuery = `
+        query GetRootFormsQuery {
+            listRootForms(filter: {formUrls: {attributeExists: false}}) {
+                items {
+                    id
+                    formUrls
+                }
+            }
+        }
+    `
+
+    const getRootFormsOptions = {
+        method: 'POST',
+        headers: {
+        'x-api-key': GRAPHQL_API_KEY,
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query: getRootFormsQuery })
+    };
+
+    const getRootFormsRequest = new Request(GRAPHQL_ENDPOINT, getRootFormsOptions);
+
+    const getRootFormsResponse = await fetch(getRootFormsRequest);
+
+    const getRootFormsBody = await getRootFormsResponse.json();
+
+    const rootForms = getRootFormsBody.data.listRootForms.items;
+
+    for (const rootForm of rootForms) {
+        const updateRootFormMulation = `
+            mutation UpdateRootFormMutation {
+                    updateRootForm(
+                    input: {id: "${rootForm.id}", formUrls: "loudoun"}
+                ) {
+                    formUrls
+                    id
+                }
+            }
+        `
+
+        const updateRootFormOptions = {
+            method: 'POST',
+            headers: {
+            'x-api-key': GRAPHQL_API_KEY,
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ query: updateRootFormMulation })
+        };
+
+        const updateRootFormRequest = new Request(GRAPHQL_ENDPOINT, updateRootFormOptions);
+
+        const updateRootFormResponse = await fetch(updateRootFormRequest);
+
+        const updateRootFormBody = await updateRootFormResponse.json();
+
+        console.log('===== Updated Root Form =====', updateRootFormBody)
+    }
+}
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
     try {
         await convertTestCycles();
+        await convertRootForms();
 
         return {
             statusCode: 200,
