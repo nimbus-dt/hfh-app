8bf719fa942b4360115152571ce421b60b5ef52b
Guillermo Avelar <guillermo.avelar@nimbus-dt.com>
2024-05-13 20:38:29 -0600

    💄add sections

--- src/layouts/LandingLayout/LandingLayout.tsx
+++ src/layouts/LandingLayout/LandingLayout.tsx
@@ -1,5 +1,45 @@
+import { Flex, View } from '@aws-amplify/ui-react';
+
 function LandingLayout() {
-  return <h1>Hye</h1>;
+  const nav = (
+    <View height="116px" border="1px solid black">
+      nav
+    </View>
+  );
+
+  const hero = (
+    <View height="1083px" border="1px solid black">
+      hero
+    </View>
+  );
+
+  const features = (
+    <View height="1112px" border="1px solid black">
+      features
+    </View>
+  );
+
+  const contact = (
+    <View height="331px" border="1px solid black">
+      contact
+    </View>
+  );
+
+  const faq = (
+    <View height="732px" border="1px solid black">
+      faq
+    </View>
+  );
+
+  return (
+    <Flex direction="column" width="100%" height="3374px" gap="0px">
+      {nav}
+      {hero}
+      {features}
+      {contact}
+      {faq}
+    </Flex>
+  );
 }
 
 export default LandingLayout;
