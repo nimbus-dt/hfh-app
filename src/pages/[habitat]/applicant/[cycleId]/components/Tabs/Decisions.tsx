dff516d0b24c36c35d4de97107ec816cbf688d00
batarse1 <00014419@uca.edu.sv>
2024-05-16 14:29:34 -0600

    ✨ Update Decisions component styling and sorting logic

--- src/pages/[habitat]/applicant/[cycleId]/components/Tabs/Decisions.tsx
+++ src/pages/[habitat]/applicant/[cycleId]/components/Tabs/Decisions.tsx
@@ -25,16 +25,23 @@ const Decisions = ({ application }: DecisionsProps) => {
   const { habitat }: IOutletContext = useOutletContext();
 
   const { data: decisions }: { data: Decision[] } = useDecisionsQuery({
-    criteria: (c2: RecursiveModelPredicate<Decision>) =>
-      c2.or((c3) => [c3.testapplicationID.eq(application.id)]),
-    dependencyArray: [application],
+    criteria: (c1: RecursiveModelPredicate<Decision>) =>
+      c1.and((c2) => [c2.testapplicationID.eq(application.id)]),
     paginationProducer: (s: SortPredicate<Decision>) =>
       s.createdAt(SortDirection.DESCENDING),
+    dependencyArray: [application],
   });
 
   return (
     <Flex className={`${style.decisionsContainer}`}>
-      {decisions.length > 0 ? (
+      {decisions.sort((a, b) => {
+        if (a.createdAt && b.createdAt) {
+          return (
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+        }
+        return 0;
+      }).length > 0 ? (
         decisions.map((data) => (
           <DecisionCard
             key={data.id}
