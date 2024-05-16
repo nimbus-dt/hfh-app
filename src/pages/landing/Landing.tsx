69ab9e3c258fd531ec9359234b5f70fb1905b892
Guillermo Avelar <guillermo.avelar@nimbus-dt.com>
2024-05-15 22:59:48 -0600

    :feature: Faqs done except accordion

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,23 +1,18 @@
-import { View, Flex } from '@aws-amplify/ui-react';
+import { Flex } from '@aws-amplify/ui-react';
 import Nav from './components/Nav';
 import Features from './components/Features';
 import Hero from './components/Hero';
 import Contact from './components/Contact';
+import Faqs from './components/Faqs';
 
 function Landing() {
-  const faq = (
-    <View height="732px" border="1px solid black">
-      faq
-    </View>
-  );
-
   return (
     <Flex direction="column" width="100%" height="fit-content" gap="0px">
       <Nav />
       <Hero />
       <Features />
       <Contact />
-      {faq}
+      <Faqs />
     </Flex>
   );
 }
---
+++ src/pages/landing/components/Faqs/Faqs.tsx
@@ -0,0 +1,48 @@
+import { Flex, Text } from '@aws-amplify/ui-react';
+
+function Faqs() {
+  return (
+    <Flex
+      direction="column"
+      gap="40px"
+      alignContent="center"
+      alignItems="center"
+      justifyContent="center"
+      padding={{ base: '56px 32px', medium: '56px 48px', large: '56px 132px' }}
+      backgroundColor="var(--amplify-colors-neutral-10)"
+    >
+      <Flex
+        width="100%"
+        height="fit-content"
+        alignItems="center"
+        gap="24px"
+        padding="0px"
+        direction="column"
+      >
+        <Text
+          fontWeight="medium"
+          fontSize={{ base: '36px', medium: '48px', large: '54px' }}
+          width="100%"
+          height="fit-content"
+          textAlign="center"
+          color="var(--amplify-colors-neutral-100)"
+        >
+          FAQs
+        </Text>
+        <Text
+          fontWeight="light"
+          fontSize="24px"
+          width="100%"
+          height="fit-content"
+          textAlign="center"
+          color="var(--amplify-colors-neutral-100)"
+        >
+          HabitatApp is here to digitally transform how affiliates conduct their
+          Homeownership Program.
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Faqs;
