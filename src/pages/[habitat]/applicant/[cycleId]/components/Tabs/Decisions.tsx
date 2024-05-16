ebd6e9ac108bbe17304a00d48c9413fd51c57c5d
batarse1 <00014419@uca.edu.sv>
2024-05-16 09:59:24 -0600

    ✨ Add Decisions tab to the applicantCyclePage

---
+++ src/pages/[habitat]/applicant/[cycleId]/components/Tabs/Decisions.tsx
@@ -0,0 +1,51 @@
+import { useOutletContext } from 'react-router-dom';
+
+import { Flex } from '@aws-amplify/ui-react';
+import {
+  RecursiveModelPredicate,
+  SortDirection,
+  SortPredicate,
+} from '@aws-amplify/datastore';
+
+import DecisionCard from 'components/DecisionCard';
+import { useDecisionsQuery } from 'hooks/services';
+import { Decision, Habitat, ReviewStatus, TestApplication } from 'models';
+
+import style from './Decisions.module.css';
+
+interface IOutletContext {
+  habitat?: Habitat;
+}
+
+interface DecisionsProps {
+  application: TestApplication;
+}
+
+const Decisions = ({ application }: DecisionsProps) => {
+  const { habitat }: IOutletContext = useOutletContext();
+
+  const { data: decisions }: { data: Decision[] } = useDecisionsQuery({
+    criteria: (c2: RecursiveModelPredicate<Decision>) =>
+      c2.or((c3) => [c3.testapplicationID.eq(application.id)]),
+    dependencyArray: [application],
+    paginationProducer: (s: SortPredicate<Decision>) =>
+      s.createdAt(SortDirection.DESCENDING),
+  });
+
+  return (
+    <Flex className={`${style.decisionsContainer}`}>
+      {decisions.map((data) => (
+        <DecisionCard
+          key={data.id}
+          date={data.updatedAt || ''}
+          habitat={habitat?.name || ''}
+          status={ReviewStatus[data?.status || 'PENDING']}
+          editorState={data.serializedEditorState}
+          showReviewButton
+        />
+      ))}
+    </Flex>
+  );
+};
+
+export default Decisions;
