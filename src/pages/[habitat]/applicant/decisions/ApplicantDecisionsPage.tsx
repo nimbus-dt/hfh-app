8f5c243cffb6c5a41e352cd0094a88480018869d
batarse1 <00014419@uca.edu.sv>
2024-05-28 19:35:53 -0600

    ✨ Update DecisionsPage fetch

--- src/pages/[habitat]/applicant/decisions/ApplicantDecisionsPage.tsx
+++ src/pages/[habitat]/applicant/decisions/ApplicantDecisionsPage.tsx
@@ -13,13 +13,21 @@ import {
 } from '@aws-amplify/datastore';
 
 import DecisionCard from 'components/DecisionCard';
-import { useDecisionsQuery, useTestApplicationsQuery } from 'hooks/services';
+import {
+  useDecisionsQuery,
+  useRootFormsQuery,
+  useTestApplicationsQuery,
+  useTestCyclesQuery,
+} from 'hooks/services';
 import {
   Decision,
   Habitat,
+  LazyRootForm,
   LazyTestApplication,
   ReviewStatus,
+  RootForm,
   TestApplication,
+  TestCycle,
 } from 'models';
 
 import style from './ApplicantDecisionsPage.module.css';
@@ -32,11 +40,49 @@ const ApplicantDecisionsPage = () => {
   const { user } = useAuthenticator((context) => [context.user]);
   const { habitat }: IOutletContext = useOutletContext();
 
+  const { data: rootForms }: { data: RootForm[] } = useRootFormsQuery({
+    criteria: (c1: RecursiveModelPredicate<LazyRootForm>) =>
+      c1.habitatID.eq(habitat?.id || ''),
+    dependencyArray: [habitat],
+    paginationProducer: (s: SortPredicate<LazyRootForm>) =>
+      s.createdAt(SortDirection.DESCENDING),
+  });
+
+  const { data: cycles }: { data: TestCycle[] } = useTestCyclesQuery({
+    criteria: (c2: RecursiveModelPredicate<TestCycle>) =>
+      c2.or((c3) => {
+        const newCycles = rootForms.map((rootForm) =>
+          c3.rootformID.eq(rootForm.id)
+        );
+
+        if (!newCycles.length) {
+          return [c3.id.eq('')];
+        }
+
+        return newCycles;
+      }),
+    dependencyArray: [rootForms],
+    paginationProducer: {
+      sort: (s: SortPredicate<TestCycle>) =>
+        s.createdAt(SortDirection.DESCENDING),
+    },
+  });
+
   const { data: applications }: { data: TestApplication[] } =
     useTestApplicationsQuery({
-      criteria: (c2: RecursiveModelPredicate<LazyTestApplication>) =>
-        c2.ownerID.eq(user?.username),
-      dependencyArray: [user],
+      criteria: (c2: RecursiveModelPredicate<TestApplication>) =>
+        c2.or((c3) => {
+          const newApplications = cycles.map((cycle) =>
+            c3.testcycleID.eq(cycle.id)
+          );
+
+          if (!newApplications.length) {
+            return [c3.id.eq('')];
+          }
+
+          return newApplications;
+        }),
+      dependencyArray: [user, cycles],
       paginationProducer: (s: SortPredicate<LazyTestApplication>) =>
         s.createdAt(SortDirection.DESCENDING),
     });
@@ -44,9 +90,9 @@ const ApplicantDecisionsPage = () => {
   const { data: decisions }: { data: Decision[] } = useDecisionsQuery({
     criteria: (c2: RecursiveModelPredicate<Decision>) =>
       c2.or((c3) => {
-        const newDecisions = applications.map((application) =>
-          c3.testapplicationID.eq(application.id)
-        );
+        const newDecisions = applications
+          .filter((application) => application.ownerID === user?.username)
+          .map((application) => c3.testapplicationID.eq(application.id));
 
         if (!newDecisions.length) {
           return [c3.id.eq('')];
