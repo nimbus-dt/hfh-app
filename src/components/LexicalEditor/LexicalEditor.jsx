4fee189e89a62ed14f89732e64dd7e16101b5fec
Diego Quinteros <dev.quinteros@gmail.com>
2024-06-13 11:34:54 -0600

    ✨ Update LexicalEditor to be able to set custom placeholders

--- src/components/LexicalEditor/LexicalEditor.jsx
+++ src/components/LexicalEditor/LexicalEditor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
@@ -22,23 +22,48 @@ import FilePlugin from './plugins/FilePlugin';
 import { FileNode } from './nodes/FileNode';
 import ExportToHtmlPlugin from './plugins/ExportToHtmlPlugin';
 
-function Placeholder() {
+// eslint-disable-next-line react/display-name
+const Placeholder = ({ text, updateHeight }) => {
+  const textRef = useRef(null);
+  useEffect(() => {
+    if (!textRef.current) return;
+    const resizeObserver = new ResizeObserver(() => {
+      if (textRef.current) {
+        updateHeight(textRef.current.offsetHeight);
+      }
+    });
+    resizeObserver.observe(textRef.current);
+    return () => {
+      resizeObserver.disconnect();
+      updateHeight(undefined);
+    };
+  }, [updateHeight]);
+
   return (
     <Text
       opacity="50"
       position="absolute"
-      top="0.75rem"
-      left="0.5rem"
+      top="0"
+      left="0"
+      padding="0.75rem 0.5rem"
       overflow="hidden"
       style={{
+        whiteSpace: 'pre-wrap',
         textOverflow: 'ellipsis',
         pointerEvents: 'none',
       }}
+      as="p"
+      ref={textRef}
     >
-      Enter some text...
+      {text || 'Enter some text...'}
     </Text>
   );
-}
+};
+
+Placeholder.propTypes = {
+  text: PropTypes.string,
+  updateHeight: PropTypes.func,
+};
 
 const LexicalEditor = ({
   onChange = () => undefined,
@@ -46,51 +71,65 @@ const LexicalEditor = ({
   editable,
   serializedEditorState,
   disableFiles,
-}) => (
-  <LexicalComposer
-    initialConfig={{
-      theme: LexicalEditorTheme,
-      namespace: 'MyEditor',
-      onError: (error) => console.log('Lexical error', error),
-      nodes: [
-        ImageNode,
-        ...(disableFiles ? [] : [FileNode]),
-        HeadingNode,
-        ListNode,
-        ListItemNode,
-      ],
-    }}
-  >
-    <View
-      border="var(--amplify-colors-border-primary)"
-      borderStyle={editable ? 'solid' : ''}
-      borderRadius="small"
-      borderWidth="medium"
+  placeholder,
+}) => {
+  const [placeholderHeight, setPlaceholderHeight] = useState(undefined);
+  return (
+    <LexicalComposer
+      initialConfig={{
+        theme: LexicalEditorTheme,
+        namespace: 'MyEditor',
+        onError: (error) => console.log('Lexical error', error),
+        nodes: [
+          ImageNode,
+          ...(disableFiles ? [] : [FileNode]),
+          HeadingNode,
+          ListNode,
+          ListItemNode,
+        ],
+      }}
     >
-      {editable && <ToolbarPlugin disableFiles={disableFiles} />}
-      <View position="relative">
-        <RichTextPlugin
-          contentEditable={
-            <ContentEditable
-              className={`editor-input ${!editable ? 'editor-readonly' : ''}`}
-            />
-          }
-          ErrorBoundary={LexicalErrorBoundary}
-          placeholder={editable && <Placeholder />}
-        />
-        <HistoryPlugin />
-        <AutoFocusPlugin />
-        <OnChangePlugin onChange={onChange} />
-        <ListPlugin />
-        {!disableFiles && <FilePlugin />}
-        <ImagePlugin />
-        <EditablePlugin editable={!!editable} />
-        <RestoreStatePlugin serializedEditorState={serializedEditorState} />
-        {onChangeHtml && <ExportToHtmlPlugin onChange={onChangeHtml} />}
+      <View
+        border="var(--amplify-colors-border-primary)"
+        borderStyle={editable ? 'solid' : ''}
+        borderRadius="small"
+        borderWidth="medium"
+      >
+        {editable && <ToolbarPlugin disableFiles={disableFiles} />}
+        <View position="relative">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className={`editor-input ${!editable ? 'editor-readonly' : ''}`}
+                style={{
+                  height: placeholderHeight,
+                }}
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+            placeholder={
+              editable && (
+                <Placeholder
+                  text={placeholder}
+                  updateHeight={setPlaceholderHeight}
+                />
+              )
+            }
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+          <OnChangePlugin onChange={onChange} />
+          <ListPlugin />
+          {!disableFiles && <FilePlugin />}
+          <ImagePlugin />
+          <EditablePlugin editable={!!editable} />
+          <RestoreStatePlugin serializedEditorState={serializedEditorState} />
+          {onChangeHtml && <ExportToHtmlPlugin onChange={onChangeHtml} />}
+        </View>
       </View>
-    </View>
-  </LexicalComposer>
-);
+    </LexicalComposer>
+  );
+};
 
 LexicalEditor.propTypes = {
   serializedEditorState: PropTypes.string,
@@ -98,6 +137,7 @@ LexicalEditor.propTypes = {
   onChangeHtml: PropTypes.func,
   editable: PropTypes.bool,
   disableFiles: PropTypes.bool,
+  placeholder: PropTypes.string,
 };
 
 export default LexicalEditor;
