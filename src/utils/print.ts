d5a95e2126e3b29948bc6de40f4804e1c4d9bc55
Diego Quinteros <dev.quinteros@gmail.com>
2024-06-14 13:21:21 -0600

    ✨ Add printElement util function

---
+++ src/utils/print.ts
@@ -0,0 +1,39 @@
+/**
+ * Returns a string with all the clases joined by empty space
+ * @param {...string} classes
+ * @returns {string}
+ */
+
+/**
+ * Print the provided element, replacing the body content with the element
+ * after printing, the original body content is restored
+ * @param element
+ */
+export const printElement = (element: HTMLElement) => {
+  const { body } = document;
+
+  const originalChildren: Node[] = [...body.children];
+
+  const elementClone = element.cloneNode(true) as HTMLElement;
+
+  while (body.firstChild) {
+    body.firstChild.remove();
+  }
+
+  body.append(elementClone);
+
+  const anchors = body.getElementsByTagName('a');
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].setAttribute('href', '#');
+  }
+
+  window.print();
+
+  while (body.firstChild) {
+    (body.firstChild as HTMLElement).remove();
+  }
+
+  for (const child of originalChildren) {
+    body.append(child);
+  }
+};
