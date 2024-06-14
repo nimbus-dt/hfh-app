79333c8f79beb64502b2e7ecea35119044613004
batarse1 <00014419@uca.edu.sv>
2024-06-14 12:56:25 -0600

    ✨ Update breadCrumbsItems and GoBack

--- src/pages/[habitat]/affiliate/cycles/[cycleId]/AffiliateCycleApplications.tsx
+++ src/pages/[habitat]/affiliate/cycles/[cycleId]/AffiliateCycleApplications.tsx
@@ -17,7 +17,6 @@ import {
 import { useEffect, useState } from 'react';
 import {
   MdOutlineAdd,
-  MdOutlineArrowBack,
   MdOutlineFilterList,
   MdOutlineLink,
   MdOutlineOpenInNew,
@@ -33,6 +32,7 @@ import { stringToHumanReadable } from 'utils/strings';
 import IconButton from 'components/IconButton';
 import BreadCrumbs from 'components/BreadCrumbs/BreadCrumbs';
 import DropdownMenu from 'components/DropdownMenu';
+import GoBack from 'components/GoBack';
 import { DEFAULT_REVIEW_STATUS } from 'utils/constants';
 import { useBreakpointValue } from '@aws-amplify/ui-react';
 
@@ -188,28 +188,22 @@ const AffiliateCycleApplications = () => {
     navigator.clipboard.writeText(applicantLink);
   };
 
+  const breadCrumbsItems = [
+    { label: 'Homeownership Form', to: '../../forms' },
+    { label: 'Cycles', to: '..' },
+    { label: 'Applications' },
+  ];
+
   return (
     <div className={style.container}>
       <div className={style.firstRow}>
-        {!isSmall && (
-          <BreadCrumbs
-            items={[
-              { label: 'Homeownership Form', to: '../../forms' },
-              { label: 'Cycles', to: '../' },
-              { label: 'Applications' },
-            ]}
-          />
-        )}
+        {!isSmall && <BreadCrumbs items={breadCrumbsItems} />}
         <p className={`theme-body-medium ${style.incompleteApplications}`}>
           Incomplete Applications: {applicationsPending.length}
         </p>
       </div>
       <div className={`${style.titleContainer}`}>
-        <Link to="../">
-          <IconButton variation="not-outlined">
-            <MdOutlineArrowBack />
-          </IconButton>
-        </Link>
+        <GoBack />
         <span className={`theme-headline-medium ${style.title}`}>
           Applications Dashboard
         </span>
