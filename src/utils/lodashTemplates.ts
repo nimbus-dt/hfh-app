c73078495b8093352fd72bed2e2462ea58ba5dcc
Diego Quinteros <dev.quinteros@gmail.com>
2024-06-19 11:57:49 -0600

    🧑‍💻 Update lodashTemplates

--- src/utils/lodashTemplates.ts
+++ src/utils/lodashTemplates.ts
@@ -52,3 +52,101 @@ const rowTemplate = `
   {% }) %}
 </div>
 `;
+
+const templateWithEditGridChildExample = `
+<div class="row hfh_formio_hide_on_print">
+  {% util.eachComponent(components, function(component) { %}
+    {% if (displayValue(component) && (component.label == "Full legal name"||component.label == "Relationship")) { %}
+      <div class="col-sm-3">
+        {{ isVisibleInRow(component) ? getView(component, row[component.key]) : ''}}
+      </div>
+    {% } %}
+  {% }) %}
+  {% if (!instance.options.readOnly && !instance.disabled) { %}
+    <div class="col-sm-2">
+      <div class="btn-group pull-right">
+        <button class="btn btn-default btn-light btn-sm editRow"><i class="{{ iconClass('edit') }}"></i></button>
+        {% if (!instance.hasRemoveButtons || instance.hasRemoveButtons()) { %}
+          <button class="btn btn-danger btn-sm removeRow"><i class="{{ iconClass('trash') }}"></i></button>
+        {% } %}
+      </div>
+    </div>
+  {% } else { %}
+    <div class="col-sm-2">
+      <div class="btn-group pull-right">
+        <button class="btn btn-default btn-light btn-sm editRow"><i class="{{ iconClass('eye') }}"></i></button>
+      </div>
+    </div>
+  {% } %}
+</div>
+<div class="row hfh_formio_show_on_print">
+  <div class="row p-3">
+    <div class="col-12">
+      {{ components[0].label }}: {{ getView(components[0], row[components[0].key]) }}
+    </div>
+  </div>
+  <div class="row p-3">
+    <div class="col-12">
+      {{ components[1].label }}: {{ getView(components[1], row[components[1].key]) }}
+    </div>
+  </div>
+  <div class="row p-3">
+    <div class="col-12">
+      {{ components[2].label }}: {{ getView(components[2], row[components[2].key]) }}
+    </div>
+  </div>
+  <div class="row p-3">
+    <div class="col-12">
+      {{ components[3].label }}: {{ getView(components[3], row[components[3].key]) }}
+    </div>
+  </div>
+  <ul class="list-group p-3">
+    <li class="list-group-item list-group-header">
+      Income records
+    </li>
+    {% for(income of row['applicantHouseholdMembersIncomeRecords']){ %}
+      <li class="list-group-item">
+        {% util.eachComponent(components[4].components, function(component) { %}
+        <div class="row p-3">
+          <div class="col-12">
+            {{ component.label }}: {{ getView(component, income[component.key]) }}
+          </div>
+        </div>
+        {% }) %}
+      </li>
+    {% } %}
+  </ul>
+  <ul class="list-group p-3">
+    <li class="list-group-item list-group-header">
+      Debt records
+    </li>
+    {% for(debt of row['applicantHouseholdMembersDebtRecords']){ %}
+      <li class="list-group-item">
+        {% util.eachComponent(components[5].components, function(component) { %}
+        <div class="row p-3">
+          <div class="col-12">
+            {{ component.label }}: {{ getView(component, debt[component.key]) }}
+          </div>
+        </div>
+        {% }) %}
+      </li>
+    {% } %}
+  </ul>
+  <ul class="list-group p-3">
+    <li class="list-group-item list-group-header">
+      Assets records
+    </li>
+    {% for(asset of row['applicantHouseholdMembersAssetRecords']){ %}
+      <li class="list-group-item">
+        {% util.eachComponent(components[6].components, function(component) { %}
+        <div class="row p-3">
+          <div class="col-12">
+            {{ component.label }}: {{ getView(component, asset[component.key]) }}
+          </div>
+        </div>
+        {% }) %}
+      </li>
+    {% } %}
+  </ul>
+</div>
+`;
