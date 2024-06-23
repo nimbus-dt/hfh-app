fac7c52761f76dbc1d687069e5d2466f11cce383
batarse1 <00014419@uca.edu.sv>
2024-06-22 21:16:38 -0600

    ✨ Add Username component for displaying user's name

---
+++ src/pages/[habitat]/affiliate/cycles/[cycleId]/components/Username/Username.tsx
@@ -0,0 +1,33 @@
+import { DataStore } from 'aws-amplify';
+import useAsync from 'hooks/utils/useAsync/useAsync';
+import { TestApplication, User } from 'models';
+import { useCallback } from 'react';
+
+interface UsernameProps {
+  application: TestApplication;
+}
+
+const Username = ({ application }: UsernameProps) => {
+  const getUsername = useCallback(async () => {
+    try {
+      const ownerId = application.ownerID;
+      if (!(typeof ownerId === 'string')) return 'unknown';
+
+      const user = await DataStore.query(User, (c1) => c1.owner.eq(ownerId));
+
+      const currentUser = user[0];
+      if (!currentUser) return 'unknown';
+      return `${currentUser.firstName} ${currentUser.lastName}` || 'unknown';
+    } catch (e) {
+      return 'unknown';
+    }
+  }, [application]);
+
+  const { value } = useAsync({
+    asyncFunction: getUsername,
+  });
+
+  return value;
+};
+
+export default Username;
