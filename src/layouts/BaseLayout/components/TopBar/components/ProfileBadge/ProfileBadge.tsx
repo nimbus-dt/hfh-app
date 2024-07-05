a1626dc38046186bbda548815283dd1692b79eab
batarse1 <00014419@uca.edu.sv>
2024-07-05 15:01:41 -0600

    ✨ Update ProfileBadge component to use Menu and MenuItem components for profile menu

--- src/layouts/BaseLayout/components/TopBar/components/ProfileBadge/ProfileBadge.tsx
+++ src/layouts/BaseLayout/components/TopBar/components/ProfileBadge/ProfileBadge.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-
 import { Auth } from 'aws-amplify';
-import { Button } from '@aws-amplify/ui-react';
+import { Button, Menu, MenuItem } from '@aws-amplify/ui-react';
 
 import style from './ProfileBadge.module.css';
 
@@ -10,38 +8,18 @@ interface IProperties {
 }
 
 const ProfileBadge = ({ initials }: IProperties) => {
-  const [openProfile, setOpenProfile] = useState(false);
-
   const onLogOut = () => {
     Auth.signOut();
   };
 
   return (
-    <div className={style.relative}>
-      <Button
-        className={`${style.profileBadge}`}
-        onClick={() => setOpenProfile((prev) => !prev)}
-      >
-        {initials || ''}
-      </Button>
-      {openProfile && (
-        <div className={style.menu}>
-          <button
-            onClick={onLogOut}
-            tabIndex={0}
-            type="button"
-            className={style.menuItem}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter' || event.key === ' ') {
-                onLogOut();
-              }
-            }}
-          >
-            Log out
-          </button>
-        </div>
-      )}
-    </div>
+    <Menu
+      trigger={
+        <Button className={`${style.profileBadge}`}>{initials || ''}</Button>
+      }
+    >
+      <MenuItem onClick={onLogOut}>Log out</MenuItem>
+    </Menu>
   );
 };
 
