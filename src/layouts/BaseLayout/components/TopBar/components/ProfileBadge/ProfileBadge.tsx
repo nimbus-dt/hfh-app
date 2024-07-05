27e30fe27ad345999ca2dd16823e040a219d52dc
batarse1 <00014419@uca.edu.sv>
2024-07-05 14:07:20 -0600

    ✨ Add profile menu with logout functionality

--- src/layouts/BaseLayout/components/TopBar/components/ProfileBadge/ProfileBadge.tsx
+++ src/layouts/BaseLayout/components/TopBar/components/ProfileBadge/ProfileBadge.tsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
+
+import { Auth } from 'aws-amplify';
 import { Button } from '@aws-amplify/ui-react';
-import React from 'react';
+
 import style from './ProfileBadge.module.css';
 
 interface IProperties {
   initials?: string;
 }
 
-const ProfileBadge = ({ initials }: IProperties) => (
-  <Button className={`${style.profileBadge}`}>{initials || ''}</Button>
-);
+const ProfileBadge = ({ initials }: IProperties) => {
+  const [openProfile, setOpenProfile] = useState(false);
+
+  const onLogOut = () => {
+    Auth.signOut();
+  };
+
+  return (
+    <div className={style.relative}>
+      <Button
+        className={`${style.profileBadge}`}
+        onClick={() => setOpenProfile((prev) => !prev)}
+      >
+        {initials || ''}
+      </Button>
+      {openProfile && (
+        <div className={style.menu}>
+          <button
+            onClick={onLogOut}
+            tabIndex={0}
+            type="button"
+            className={style.menuItem}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter' || event.key === ' ') {
+                onLogOut();
+              }
+            }}
+          >
+            Log out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ProfileBadge;
