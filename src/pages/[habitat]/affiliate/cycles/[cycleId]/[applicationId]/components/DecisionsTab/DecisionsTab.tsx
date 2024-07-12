791883decb1419a7c20d47a4ed5ba731a8ecd9cd
Diego Quinteros <dev.quinteros@gmail.com>
2024-07-12 13:32:57 -0600

    ♻️ Refactor DecisionsTab to use useHabitat instead of habitat prop

--- src/pages/[habitat]/affiliate/cycles/[cycleId]/[applicationId]/components/DecisionsTab/DecisionsTab.tsx
+++ src/pages/[habitat]/affiliate/cycles/[cycleId]/[applicationId]/components/DecisionsTab/DecisionsTab.tsx
@@ -2,32 +2,36 @@ import DecisionCard from 'components/DecisionCard';
 import { ReviewStatus, Decision, Habitat } from 'models';
 import React from 'react';
 import { Text } from '@aws-amplify/ui-react';
+import useHabitat from 'hooks/utils/useHabitat';
 import style from './DecisionsTab.module.css';
 
 interface IProperties {
-  habitat?: Habitat;
   decisions: Decision[];
 }
 
-const DecisionsTab = ({ habitat, decisions }: IProperties) => (
-  <div className={style.container}>
-    {decisions.length > 0 ? (
-      decisions.map((decision) => (
-        <DecisionCard
-          key={decision.id}
-          date={decision.createdAt || ''}
-          habitat={habitat?.longName || ''}
-          status={decision.status as keyof typeof ReviewStatus}
-          editorState={decision.serializedEditorState}
-          shouldRenderStatusChip
-        />
-      ))
-    ) : (
-      <Text textAlign="center" fontWeight="bold">
-        There are no decisions for this application
-      </Text>
-    )}
-  </div>
-);
+const DecisionsTab = ({ decisions }: IProperties) => {
+  const { habitat } = useHabitat();
+
+  return (
+    <div className={style.container}>
+      {decisions.length > 0 ? (
+        decisions.map((decision) => (
+          <DecisionCard
+            key={decision.id}
+            date={decision.createdAt || ''}
+            habitat={habitat?.longName || ''}
+            status={decision.status as keyof typeof ReviewStatus}
+            editorState={decision.serializedEditorState}
+            shouldRenderStatusChip
+          />
+        ))
+      ) : (
+        <Text textAlign="center" fontWeight="bold">
+          There are no decisions for this application
+        </Text>
+      )}
+    </div>
+  );
+};
 
 export default DecisionsTab;
