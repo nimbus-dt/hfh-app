c3532982abd21a927b40f97dadb68ad28b1a19c1
Diego Quinteros <dev.quinteros@gmail.com>
2024-08-07 18:10:51 -0600

    ✨ Update HabitatLayout to work with habitat loader and errorElement

--- src/layouts/HabitatLayout/HabitatLayout.tsx
+++ src/layouts/HabitatLayout/HabitatLayout.tsx
@@ -1,63 +1,16 @@
-import { Outlet, useParams } from 'react-router-dom';
+import { Await, Outlet, useLoaderData, useParams } from 'react-router-dom';
 import { Authenticator, Flex, Text } from '@aws-amplify/ui-react';
 import CheckMaintenance from 'layouts/Maintenance/CheckMaintenance';
 import useHabitat from 'hooks/utils/useHabitat';
-import { useEffect, useState } from 'react';
-import { DataStore } from 'aws-amplify/datastore';
+import { Suspense, useEffect } from 'react';
 import { Habitat } from 'models';
 
 const HabitatLayout = () => {
-  const { habitat, setHabitat } = useHabitat();
+  const { habitat } = useLoaderData() as { habitat: Habitat };
 
-  const [isLoading, setIsLoading] = useState(0);
+  const { setHabitat } = useHabitat();
 
-  const { habitat: habitatUrlName } = useParams();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading((prevIsLoading) => prevIsLoading + 1);
-        const habitatsResponse = await DataStore.query(Habitat, (c) =>
-          c.urlName.eq(habitatUrlName)
-        );
-
-        const habitatObject = habitatsResponse[0];
-
-        setHabitat(habitatObject);
-        setIsLoading((prevIsLoading) => prevIsLoading - 1);
-      } catch (error) {
-        console.log(`Error fetching habitat: ${error}`);
-      }
-    };
-
-    fetchData();
-  }, [habitatUrlName, setHabitat]);
-
-  if (isLoading) {
-    return (
-      <Flex
-        direction="column"
-        height="100vh"
-        alignItems="center"
-        padding="16px"
-      >
-        <Text>Loading...</Text>
-      </Flex>
-    );
-  }
-
-  if (!habitat) {
-    return (
-      <Flex
-        direction="column"
-        height="100vh"
-        alignItems="center"
-        padding="16px"
-      >
-        <Text>Habitat not found.</Text>
-      </Flex>
-    );
-  }
+  useEffect(() => setHabitat(habitat), [habitat, setHabitat]);
 
   return (
     <CheckMaintenance>
