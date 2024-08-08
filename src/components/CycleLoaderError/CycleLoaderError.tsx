e2ee516a2af3d663a94d6aee39a5573682369a42
Diego Quinteros <dev.quinteros@gmail.com>
2024-08-08 15:06:23 -0600

    ✨ Add CycleLoaderError component

---
+++ src/components/CycleLoaderError/CycleLoaderError.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import LoaderError from 'components/LoaderError';
+import { useTranslation } from 'react-i18next';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import styles from './CycleLoaderError.module.css';
+
+const CycleLoaderError = () => {
+  const error = useRouteError();
+
+  const { t } = useTranslation();
+
+  let message = t(
+    'pages.habitat.affiliate.cycles.cycle.errors.default.message'
+  );
+  let hideRetry = false;
+
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    message = t('pages.habitat.affiliate.cycles.cycle.errors.404.message');
+    hideRetry = true;
+  }
+
+  return (
+    <div className={styles.container}>
+      <LoaderError message={message} hideRetry={hideRetry} />
+    </div>
+  );
+};
+
+export default CycleLoaderError;
